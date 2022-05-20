3e0870b9db0c9f970a2435f50c4fd659af4a3290
Melanie <you know how to contact me>
2022-05-21 00:45:34 +0200

    Added Slur Detection

--- main.py
+++ main.py
@@ -17,73 +17,140 @@ for the checkForMods function as it is much
 improved upon my initial version of it"""
 
 def checkForMods(messageData):
-  modRoles=["<@&856424878437040168>","<@&963537133589643304>","<@&875805670799179799>","<@&883255791610638366>"]
-  modDetected=False
-  
-  for modmention in modRoles:
-    if modmention in messageData:
-      modDetected=True
+	modRoles=["<@&856424878437040168>","<@&963537133589643304>","<@&875805670799179799>","<@&883255791610638366>"]
+	modDetected=False
+	
+	for modmention in modRoles:
+		if modmention in messageData:
+			modDetected=True
 
-  return (modDetected)
+	return modDetected
 
 def isMod(userRoles):
-  modRolePresent=False
-  for role in userRoles:
-    print(str(role.id))
-    if "856424878437040168" == str(role.id):
-      modRolePresent=True
-    elif "883255791610638366" == str(role.id):
-      modRolePresent=True
-  return (modRolePresent)
+	modRolePresent=False
+	for role in userRoles:
+		print(str(role.id))
+		if "856424878437040168" == str(role.id):
+			modRolePresent=True
+		elif "883255791610638366" == str(role.id):
+			modRolePresent=True
+	return (modRolePresent)
 
 @bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(bot))
+	print('We have logged in as {0.user}'.format(bot))
 
 @bot.command()
 async def offences(ctx):
-  if isMod(ctx.author.roles):
-    offenceOutput=str("")
-    for i in range (len(offenceList)):
-      if i == 0:
-        offenceOutput=offenceOutput+offenceList[i]
-      else:
-        offenceOutput=offenceOutput+str("\n")+str(offenceList[i])
-    await ctx.send("__**Adam Something Central Offence List**__\n"+str(offenceOutput))
-  else:
-    await ctx.send ("Only moderators can use this command.")
+	if isMod(ctx.author.roles):
+		offenceOutput=str("")
+		for i in range (len(offenceList)):
+			if i == 0:
+				offenceOutput=offenceOutput+offenceList[i]
+			else:
+				offenceOutput=offenceOutput+str("\n")+str(offenceList[i])
+		await ctx.send("__**Adam Something Central Offence List**__\n"+str(offenceOutput))
+	else:
+		await ctx.send ("Only moderators can use this command.")
 
 @bot.command()
 async def offencecheck(ctx,offence):
-  if isMod(ctx.author.roles):
+	if isMod(ctx.author.roles):
+		
+		else:
+			ctx.send ("Only moderators can use this command.")
+
+#cleaning up the message by eliminating special characters and making the entire message lowercase    
+def clearString(string):
+    special_characters = ['!', '#', '$', '%', '&', '@', '[', ']', ' ', ']', '_', '-']
     
-    else:
-      ctx.send ("Only moderators can use this command.")
-  
+    string = string.lower()
+	for char in special_characters:
+		string = string.replace(char, '')
+    
+    return string
+
+def detectSlur(messageData):
+	#known slurs and safe words; both lists can be amended freely
+	#WORDS MUST BE LOWERCASE
+	slurs = ["spick", "coon", "trann", "fag", "retard", "nigg", "kike", "yid", "chink", "gook", "negro", "shemale", "e621", "killyourself", "kys", "gypsy", "<@!809891646606409779>", "spastic", "spas", "spaz", "darky", "gippo", "paki", "spic", "dyke", "shizo", "mong"]
+	goodword = ["skyscraper", "montenegro", "pakistan", "spicy", "among", "mongrel", "schizophrenia", "zelenskys", "tycoon", "suspicious", "racoon", "yiddish"]
+	
+	messageData = clearString(messageData)
+	
+	#if a slur is detected, we increase by 1, if a word that cointains a slur but is safe is found, you get a free pass (substracting that increment), any slur that is not exused that way will be reported
+	slur_counter = 0 #more like based_counter, amirite?
+    
+	for slur in slurs:
+		slur_counter += messageData.count(slur)
+	for word in goodword:
+		slur_counter -= messageData.count(word)
+
+	return slur_counter
+	
+## I sure hope My pakistani friends and myself will be able to enjoy our spicy noodles among the beautiful skyscrapers of Montenegro	--Pando	
 
 @bot.event
 async def on_message(message):
-  modCheckOutput=(checkForMods(message.content))
-  if message.author == bot.user:
-    return
+	slur_heat = detectSlur(message.content)
+	
+	if message.author == client.user: #ignores message if message is by bot
+		return
 
-  if modCheckOutput == True:
-    embedVar = discord.Embed(title="Moderator Ping Acknowledgment", description=(message.author.mention)+" moderators have been notified of your ping and will investigate when able to do so.", color=discord.Color.from_rgb(237,91,6))
-    await message.channel.send(embed=embedVar)
-    channel = bot.get_channel(897874682198511648)
-    embedVar = discord.Embed(title="Moderator Ping", description= "A moderation role has been pinged, please investigate the ping and take action as appropriate.\n\n__Channel:__\n"+str(message.channel.mention)+"\n\n__User:__\n"+str(message.author.mention)+"\n\n__Context:__\n"+str(message.content)+"\n\n__URL:__\n"+str(message.jump_url), color=discord.Color.from_rgb(237,91,6))
-    await channel.send(embed=embedVar)
-    #await channel.send("Moderators have been pinged in "+str(message.channel)+".\nPlease investigate if you are available to do so.")
+	elif checkForMods(message.content): #checks moderator ping
+	
+		#reply to user
+		embedVar = discord.Embed(
+			title="Moderator Ping Acknowledgment", 
+			description=(message.author.mention)+" moderators have been notified of your ping and will investigate when able to do so.", 
+			color=discord.Color.from_rgb(237,91,6))
+		embedVar.set_footer(text="Ping detection written by Hekkland and Melanie")
+		await message.channel.send(embed=embedVar)
+		
+		#notification for mods
+		channel = client.get_channel(897874682198511648)
+		embedVar = discord.Embed(
+			title="Moderator Ping", 
+			description="A moderation role has been pinged, please investigate the ping and take action as appropriate.\n\n__Channel:__\n"
+				+str(message.channel.mention)
+				+"\n\n__User:__\n"
+				+str(message.author.mention)
+				+"\n\n__Context:__\n"
+				+str(message.content)
+				+"\n\n__URL:__\n"
+				+str(message.jump_url), 
+			color=discord.Color.from_rgb(237,91,6))
+		embedVar.set_footer(text="Ping detection written by Hekkland and Melanie")
+		await channel.send(embed=embedVar)
+	
+	elif slur_heat > 0: #checks slur heat
+		#notification for mods
+		channel = client.get_channel(897874682198511648)
+		embedVar = discord.Embed(
+			title="Slur(s) Detected", 
+			description="A slur has been detected. Moderation action is advised\n\n__Channel:__\n"
+				+str(message.channel.mention)
+				+"\n\n__User:__\n"
+				+str(message.author.mention)
+				+"\n\n__Context:__\n"
+				+str(message.content)
+				+"\n\n__Slur Heat:__\n"
+				+str(slur_heat)
+				+"\n\n__URL:__\n"
+				+str(message.jump_url), 
+			color=discord.Color.from_rgb(237,91,6))
+		embedVar.set_footer(text="Slur detection written by Hekkland and Melanie")
+		await channel.send(embed=embedVar)
 
-  """"if message.content.startswith ("mb!offences"):
-    modRolePresent=(isMod(user.roles))
-    if modRolePresent==True:
-      channel.message.send("The test was successful!")
-    else:
-      channel.message.send("Only moderators can use this command.")"""
+	""""if message.content.startswith ("mb!offences"):
+		modRolePresent=(isMod(user.roles))
+		if modRolePresent==True:
+			channel.message.send("The test was successful!")
+		else:
+			channel.message.send("Only moderators can use this command.")"""
 
-  await bot.process_commands(message)
+	await bot.process_commands(message)
 
-    
+		
 keep_alive()
 bot.run(os.environ['rtoken'])
