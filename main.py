4f74c67add5172b9b007c6b5b99cbe57fe8cf91c
Yoshod <stephenkyle1502@hotmail.co.uk>
2022-05-21 12:56:28 +0100

    Improved slur detection

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import os
 import random
+from itertools import product 	#needed for slur obscurity permutations
+import unidecode				#needed for cleaning accents and diacritic marks
 from discord import DMChannel
 from discord.ext import commands
+slurs = []
 
 intents = discord.Intents.all()
 intents.members = True
@@ -10,6 +13,28 @@ intents.members = True
 bot = commands.Bot(command_prefix="mb!", intents=intents)
 
 offenceList=["Intentional Bigotry","Unintentional Bigotry","Spam","NSFW Content","Channel Misuse"]
+
+def leet(word):
+	substitutions = {
+		"a": ("a", "@", "*", "4"),
+		"i": ("i", "*", "l", "1"),
+		"o": ("o", "*", "0", "@"),
+		"u": ("u", "*", "v"),
+		"v": ("v", "*", "u"),
+		"l": ("l", "1"),
+		"e": ("e", "*", "3", "€"),
+		"s": ("s", "$", "5"),
+		"t": ("t", "7"),
+		"y": ("y", "¥"),
+		"n": ("n", "и"),
+	}
+	possibles = []
+	for char in word.lower():
+		options = substitutions.get(char, char)
+		possibles.append(options)
+		
+	#print(possibles)
+	return [''.join(permutations) for permutations in product(*possibles)]
 
 """Thanks to @kisachi#7272 (348142492245426176)
 for the checkForMods function as it is much
@@ -28,17 +53,28 @@ def checkForMods(messageData):
 def isMod(userRoles):
 	modRolePresent=False
 	for role in userRoles:
-		print(str(role.id))
 		if "856424878437040168" == str(role.id):
 			modRolePresent=True
 		elif "883255791610638366" == str(role.id):
 			modRolePresent=True
+		elif "977394150494326855" == str(role.id):
+			modRolePresent=True
+	print(modRolePresent)
 	return (modRolePresent)
 
 @bot.event
 async def on_ready():
+	files = [f for f in os.listdir('.') if os.path.isfile(f)]
+	for f in files:
+		#print (files)
+	with open("slurs.txt", "r") as file:
+		for line in file:
+			line = line.replace('\n', '')
+			slurs.extend(leet(line))
+	#print(slurs) #will print the very long list, feel free to comment out for performance reasons
+
 	print('We have logged in as {0.user}'.format(bot))
-	await bot.change_presence(activity=discord.Game('Ban The Tankie'))
+	await bot.change_presence(activity=discord.Game('OwO observes you~~~'))
 
 @bot.command()
 async def offences(ctx):
@@ -53,30 +89,46 @@ async def offences(ctx):
 	else:
 		await ctx.send ("Only moderators can use this command.")
 
+@bot.command()
+async def dmTest(ctx,target,*,message):
+	if isMod (ctx.author.roles):
+		user=bot.get_user_info(target.id)
+		print(target.id)
+		print(user)
+		print(target)
+		await user.send(message)
+
 #cleaning up the message by eliminating special characters and making the entire message lowercase    
 def clearString(string):
-	special_characters = ['!', '#', '$', '%', '&', '@', '[', ']', ' ', ']', '_', '-']
-	
+	special_characters = ['!', '#', '%', '&', '[', ']', ' ', ']', '_', '-', '<', '>']
+
 	string = string.lower()
+	string = unidecode.unidecode(string)
+	
 	for char in special_characters:
 		string = string.replace(char, '')
+	
 	return string
 
 def detectSlur(messageData):
 	#known slurs and safe words; both lists can be amended freely
 	#WORDS MUST BE LOWERCASE
-	slurs = ["spick", "coon", "trann", "fag", "retard", "nigg", "kike", "yid", "chink", "gook", "negro", "shemale", "e621", "killyourself", "kys", "gypsy", "<@!809891646606409779>", "spastic", "spas", "spaz", "darky", "gippo", "paki", "spic", "dyke", "shizo", "mong"]
+	#slurs = ["spick", "coon", "trann", "fag", "retard", "nigg", "kike", "yid", "chink", "gook", "negro", "shemale", "e621", "killyourself", "kys", "gypsy", "809891646606409779", "spastic", "spas", "spaz", "darky", "gippo", "paki", "spic", "dyke", "shizo", "mong"]
 	goodword = ["skyscraper", "montenegro", "pakistan", "spicy", "among", "mongrel", "schizophrenia", "zelenskys", "tycoon", "suspicious", "racoon", "yiddish"]
 	
-	messageData = clearString(messageData)
-	
+	cleanedMessageData = clearString(messageData)
+	messageData = messageData.lower()
+	messageData = messageData.replace(' ', '')
+	#print("scan:", messageData, cleanedMessageData)
 	#if a slur is detected, we increase by 1, if a word that cointains a slur but is safe is found, you get a free pass (substracting that increment), any slur that is not exused that way will be reported
 	slur_counter = 0 #more like based_counter, amirite?
-    
+	
 	for slur in slurs:
 		slur_counter += messageData.count(slur)
+		slur_counter += cleanedMessageData.count(slur)
 	for word in goodword:
 		slur_counter -= messageData.count(word)
+		slur_counter -= cleanedMessageData.count(word)
 
 	return slur_counter
 	
@@ -151,5 +203,4 @@ async def on_message(message):
 
 	await bot.process_commands(message)
 
-
-bot.run("CODE HIDDEN")
+bot.run("CODE HERE")
