57b7e5ee3e82e5b24cf312b0ae72cd0fc3a9e8e9
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2022-05-25 22:15:34 +0200

    adjusted version and build number

--- cogs/about.py
+++ cogs/about.py
@@ -8,8 +8,8 @@ class About(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.verNum = str("`3.1.3`")
-        self.buildNum = str("`Build 00174`")
+        self.verNum = str("`3.1.4`")
+        self.buildNum = str("`Development Build 00181`")
         self.authorsList = load_authors()
 
     @commands.command()
