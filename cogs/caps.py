226fa27e5adcd153af70516d2ad06cfd7e8a2b0c
melanie <you know how to contact me>
2022-05-25 21:27:04 +0200

    remove print statements

--- cogs/caps.py
+++ cogs/caps.py
@@ -15,7 +15,7 @@ class Caps(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         msg_string = message.content
-        print("onmessagepenis")
+
         if len(msg_string) > self.MIN_CHARS_FOR_DETECTION:
             # remove nums and non-alpanumeric
             msg_string = unidecode.unidecode(msg_string)
@@ -25,12 +25,11 @@ class Caps(commands.Cog):
 
             if len(new_msg_string) != 0:
                 percentage = uppercase / len(new_msg_string)
-                print("ö")
+
                 if percentage > 0.7:
-                    print("Caps detected; opinion rejected")
                     # await replace(self, message, message.author, msg_string)
                     await message.delete(delay=None)
-                    print("q")
+
                     channel_webhooks = await message.channel.webhooks()
 
                     for wh in channel_webhooks:
@@ -41,20 +40,18 @@ class Caps(commands.Cog):
                         webhook = await message.channel.create_webhook(name="caps webhook by sersi")
 
                     replacement_message = await webhook.send(str(msg_string.lower()), username=message.author.display_name, avatar_url=message.author.display_avatar.url, wait=True)
-                    print("e")
+
                     channel = self.bot.get_channel(getLoggingChannel(message.guild.id))
                     logging_embed = nextcord.Embed(
                         title=f"Caps Lock Message replaced",
                         description="",
                         color=nextcord.Color.from_rgb(237, 91, 6)
                     )
-                    print("r")
                     logging_embed.add_field(name="User:", value=message.author.mention, inline=False)
                     logging_embed.add_field(name="Channel:", value=message.channel.mention, inline=False)
                     logging_embed.add_field(name="Original Message:", value=msg_string, inline=False)
                     logging_embed.add_field(name="Replacement Message:", value=str(msg_string.lower()), inline=False)
                     logging_embed.add_field(name="Link to Replacement Message:", value=f"[Jump!]({replacement_message.jump_url})", inline=True)
-                    print("a")
                     await channel.send(embed=logging_embed)
 
 
