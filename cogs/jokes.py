a2a33d2f1e38b1e4e52bd3074cd0378fab4cef22
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2022-05-26 00:23:30 +0200

    made jokes non-case sensitive

--- cogs/jokes.py
+++ cogs/jokes.py
@@ -14,28 +14,28 @@ class Jokes(commands.Cog):
         if message.author == self.bot.user:  # ignores message if message is by bot
             return
 
-        elif "pythonic" in str(message.content):
+        elif "pythonic" in str(message.content).lower():
             randomValue = random.randint(1, 10)
             if randomValue == 10:
                 await message.channel.send("Is your code even pep8 compliant bro?\nNo? It's not?\nAnd you have the gall to call yourself a programmer. Go read the contents of the pep8 style guide cover to cover you heathen.")
             else:
                 return
 
-        elif str(message.content) == "literally 1984" or str(message.content) == "Literally 1984":
+        elif str(message.content).lower() == "literally 1984":
             randomValue = random.randint(1, 10)
             if randomValue >= 9:
                 await message.channel.send("Oh my god, so true. It literally is like George Orlando's 1812")
             else:
                 return
 
-        elif str(message.content) == "nya" or str(message.content) == "Nya":
+        elif str(message.content).lower() == "nya":
             randomValue = random.randint(1, 10)
             if randomValue == 10:
                 await message.channel.send(f"Nya... nya? What are you, a fucking weeb {message.author.mention}?")
             else:
                 return
 
-        elif str(message.content) == "meow" or str(message.content) == "Meow":
+        elif str(message.content).lower() == "meow":
             randomValue = random.randint(1, 10)
             if randomValue == 10:
                 await message.channel.send(f"Meow meow meow, we get it you have a prissy attitude {message.author.mention}, we already noticed.")
