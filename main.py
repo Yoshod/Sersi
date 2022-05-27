7fbcfa475a4931dd13bacc4a969daaebda57361c
melanie <you know how to contact me>
2022-05-27 18:08:41 +0200

    new message cog

--- main.py
+++ main.py
@@ -94,46 +94,6 @@ async def ping(ctx):
 ### DEBUG AND MISC COMMANDS ###
 
 
-@bot.command()
-async def dmTest(ctx, userId=None, *, args=None):
-    if isMod(ctx.author.roles):
-        if userId is not None and args is not None:
-            target = userId
-            targetId = "Null"
-            for i in range(len(target)):
-                currentChar = target[i]
-                charTest = currentChar.isdigit()
-                print(charTest)
-                if charTest is True and targetId != "Null":
-                    targetId = str(targetId) + str(target[i])
-                    print("Character is number")
-                elif charTest is True and targetId == "Null":
-                    targetId = str(target[i])
-            targetId = int(targetId)
-            user = bot.get_user(targetId)
-            try:
-                await user.send(args)
-
-            except:
-                await ctx.send("The message failed to send. Reason: Could not DM user.")
-
-            # Logging
-            channel = bot.get_channel(getLoggingChannel(ctx.message.guild.id))
-            embedVar = nextcord.Embed(
-                title="DM Sent",
-                description="A DM has been sent.\n\n__Sender:__\n{ctx.author.mention}\n\n__Recipient:__\n{userId}\n\n__Message Content:__\n{args}",
-                color=nextcord.Color.from_rgb(237, 91, 6))
-            await channel.send(embed=embedVar)
-
-        elif userId is None and args is not None:
-            await ctx.send("No user was specified.")
-
-        elif userId is not None and args is None:
-            await ctx.send("No message was specified.")
-        else:
-            await ctx.send("How the fuck did this error appear?")
-    else:
-        await ctx.send("<:sersifail:979070135799279698> Only moderators can use this command.")
 
 ### BOT EVENTS ###
 
