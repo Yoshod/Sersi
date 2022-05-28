a0b87a1425b5409b5f448e290781d309ddacf542
melanie <you know how to contact me>
2022-05-28 14:45:43 +0200

    addtrialmod()
    feasibility test

--- cogs/moderators.py
+++ cogs/moderators.py
@@ -3,9 +3,18 @@ from nextcord.ext import commands
 # Doesn't do anything yet, got plans for this
 
 
+
 class Moderators(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def addtrialmod(self, ctx, member: nextcord.Member):
+        is_blacklisted = await ctx.invoke(self.bot.get_command('checkblacklist'), member=member)
+        if is_blacklisted:
+            ctx.send(f"Member {member} cannot be given Trial Mod! Reason: Is on blacklist")
+            return
+        
 
 
 def setup(bot):
