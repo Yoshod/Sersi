b6a0e7d0b9d1fcd822cabe8c5a4324fd9b754e21
melanie <you know how to contact me>
2022-05-28 19:44:58 +0200

    mass moving members to and from VC

---
+++ cogs/voice.py
@@ -0,0 +1,31 @@
+import nextcord
+from nextcord.ext import commands
+from baseutils import *
+# Doesn't do anything yet, got plans for this
+
+
+class Voice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def massmove(self, ctx, current: nextcord.VoiceChannel, target: nextcord.VoiceChannel):
+        memberlist = ""
+        for member in current.members:
+            memberlist = memberlist + f"**{member.display_name}** ({member.id})\n"
+            await member.move_to(channel=target, reason=f"Mass move by {ctx.author} ({ctx.author.id})")
+
+        # logging
+        embed = nextcord.Embed(
+            title="Members mass moved to other VC",
+            color=nextcord.Color.from_rgb(237, 91, 6)
+        )
+        embed.add_field(name="Moderator:", value=ctx.author.mention, inline=False)
+        embed.add_field(name="Members Moved:", value=memberlist, inline=False)
+
+        channel = ctx.guild.get_channel(getLoggingChannel(ctx.guild.id))
+        await channel.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Voice(bot))
