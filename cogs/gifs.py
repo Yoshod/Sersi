a8eb0cc9d2b9f816a145cd3ef09e904a230dda03
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2022-06-15 18:33:28 +0200

    gif logging fix

--- cogs/gifs.py
+++ cogs/gifs.py
@@ -4,7 +4,6 @@ from baseutils import *
 
 
 class Gif(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
         self.filename = "gifblacklist.txt"
@@ -30,38 +29,40 @@ class Gif(commands.Cog):
             return
 
         with open(self.filename, "a") as file:
-            file.write(url)
-            file.write("\n")
+            file.write(f"{url}\n")
         self.list = self.loadlist()
 
         # logging
         embed = nextcord.Embed(
             title="GIF added to blacklist",
             color=nextcord.Colour.from_rgb(237, 91, 6))
-        embed.add_field(name="Moderator", value=message.author.mention, inline=False)
+        embed.add_field(name="Moderator:", value=ctx.author.mention, inline=False)
         embed.add_field(name="GIF URL:", value=url, inline=False)
 
         channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'logging'))
-        channel.send(embed=embed)
+        await channel.send(embed=embed)
 
     @commands.command()
     async def removegif(self, ctx, url):
         if url in self.list:
             self.list.remove(url)
 
-        with open("goodword.txt", "w") as file:
-            for line in self.list:
-                file.write(f"{line}\n")
+        print(f"list before file write {self.list}")
+
+        with open(self.filename, "w") as file:
+            for gif in self.list:
+                print(f"current GIF {gif}")
+                file.write(f"{gif}\n")
 
         # logging
         embed = nextcord.Embed(
             title="GIF removed from blacklist",
             color=nextcord.Colour.from_rgb(237, 91, 6))
-        embed.add_field(name="Moderator", value=message.author.mention, inline=False)
+        embed.add_field(name="Moderator:", value=ctx.author.mention, inline=False)
         embed.add_field(name="GIF URL:", value=url, inline=False)
 
         channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'logging'))
-        channel.send(embed=embed)
+        await channel.send(embed=embed)
 
     @commands.command()
     async def listgifs(self, ctx):
@@ -80,12 +81,12 @@ class Gif(commands.Cog):
             embed = nextcord.Embed(
                 title="Blacklisted GIF deleted",
                 color=nextcord.Colour.from_rgb(237, 91, 6))
-            embed.add_field(name="Message Author", value=message.author.mention, inline=False)
-            embed.add_field(name="Channel", value=message.channel.mention, inline=False)
-            embed.add_field(name="Message", value=message.content, inline=False)
+            embed.add_field(name="Message Author:", value=message.author.mention, inline=False)
+            embed.add_field(name="Channel:", value=message.channel.mention, inline=False)
+            embed.add_field(name="Message:", value=message.content, inline=False)
 
-            channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'logging'))
-            channel.send(embed=embed)
+            channel = self.bot.get_channel(get_config_int('CHANNELS', 'logging'))
+            await channel.send(embed=embed)
 
 
 def setup(bot):
