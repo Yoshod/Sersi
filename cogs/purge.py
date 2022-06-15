4f78ce17c7fc372fb29fbd01d8119e428d50fd08
Yoshod <stephenkyle1502@hotmail.co.uk>
2022-06-15 15:11:15 +0100

    Added purge until

--- cogs/purge.py
+++ cogs/purge.py
@@ -14,7 +14,7 @@ class Purge(commands.Cog):
         self.MAXMESSAGES = 100
         self.MAXTIME = 15
 
-    @commands.command(aliases=['p', 'massdelete', 'delete', 'del'])
+    @commands.command(aliases=['p', 'massdelete', 'delete', 'del', 'purge'])
     async def clear(self, ctx, amount=None, member=None):
         try:
             amount = int(amount)
@@ -49,7 +49,7 @@ class Purge(commands.Cog):
             await ctx.channel.purge(limit=amount + 1, check=user_check)
 
         # LOGGING
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
         async for entry in ctx.guild.audit_logs(limit=1, action=nextcord.AuditLogAction.message_bulk_delete):
             deletion_count = entry.extra['count']
 
@@ -70,7 +70,7 @@ class Purge(commands.Cog):
         channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'modlogs'))
         await channel.send(embed=logging)
 
-    @commands.command(aliases=['tp', 'timed purge'])
+    @commands.command(aliases=['tp', 'timedpurge'])
     @commands.cooldown(1, 900, commands.BucketType.user)
     async def timed_purge(self, ctx, time=None, target=None):
         try:
@@ -105,6 +105,7 @@ class Purge(commands.Cog):
             await ctx.channel.purge(limit=10 * time, check=lambda x: True if (message.author.id == target.id) else False, after=after)
 
         # LOGGING
+        await asyncio.sleep(2)
         async for entry in ctx.guild.audit_logs(limit=1, action=nextcord.AuditLogAction.message_bulk_delete):
             deletion_count = entry.extra['count']
 
@@ -118,6 +119,38 @@ class Purge(commands.Cog):
         logging.add_field(name="Channel Purged:", value=ctx.channel.mention, inline=False)
         if target is not None:
             logging.add_field(name="User Targeted:", value=target)
+
+        channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'logging'))
+        await channel.send(embed=logging)
+
+        channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'modlogs'))
+        await channel.send(embed=logging)
+
+    @commands.command(aliases=['pu', 'purgeuntil'])
+    async def purge_until(self, ctx, message_id=None):
+        channel = ctx.message.channel
+        try:
+            message = await channel.fetch_message(int(message_id))
+        except nextcord.errors.NotFound:
+            await ctx.send(f"{self.sersifail} The message specified has not been found.")
+            return
+
+        await ctx.message.delete()
+        await channel.purge(after=message)
+
+        # LOGGING
+        await asyncio.sleep(2)
+        async for entry in ctx.guild.audit_logs(limit=1, action=nextcord.AuditLogAction.message_bulk_delete):
+            deletion_count = entry.extra['count']
+
+        logging = nextcord.Embed(
+            title="Messages Purged",
+            color=nextcord.Color.from_rgb(237, 91, 6)
+        )
+        logging.add_field(name="Moderator:", value=ctx.author.mention, inline=False)
+        logging.add_field(name="Message Specified:", value=message.jump_url, inline=False)
+        logging.add_field(name="Messages Purged:", value=(f"{int(deletion_count) - 1}"), inline=False)
+        logging.add_field(name="Channel Purged:", value=ctx.channel.mention, inline=False)
 
         channel = ctx.guild.get_channel(get_config_int('CHANNELS', 'logging'))
         await channel.send(embed=logging)
