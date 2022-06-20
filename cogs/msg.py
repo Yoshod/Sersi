c7fbbde6e0d11c81b5779323444c0a35a325cf07
melanie <76996779+kisachi5@users.noreply.github.com>
2022-06-20 18:45:44 +0200

    undo mistake

--- cogs/msg.py
+++ cogs/msg.py
@@ -1,7 +1,10 @@
 import nextcord
-
+import uuid
+import pickle
 from nextcord.ext import commands
-from baseutils import *
+from configutils import *
+from permutils import permcheck
+
 
 
 class Messages(commands.Cog):
@@ -34,24 +37,97 @@ class Messages(commands.Cog):
         logging.add_field(name="Message Content:", value=message, inline=False)
         await channel.send(embed=logging)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if not get_config_bool("MSG", "forward dms", False):
+    @commands.command(aliases=['da', 'deanonymize'])
+    async def deanonymise(self, ctx, id_num, *, reason=""):
+        if not await permcheck(ctx, is_dark_mod):
+            return
+        elif reason == "":
+            await ctx.send(f"{self.sersifail} Please provide a reason!")
             return
 
-        dm_channel = self.bot.get_channel(get_config_int("CHANNELS", "dm forward"))
-        channel_webhooks = await dm_channel.webhooks()
-        msg_sent = False
+        secretlist = {}
+        with open(self.filename, 'rb') as f:
+            try:
+                secretlist = pickle.load(f)
+            except EOFError:
+                pass
 
-        for webhook in channel_webhooks:                  # tries to find existing webhook
-            if webhook.name == "dm webhook by sersi":
-                await webhook.send(message.content, username=message.author.display_name, avatar_url=message.author.display_avatar.url)
-                msg_sent = True
+        try:
+            member_snowflake = secretlist[id_num]
+        except KeyError:
+            await ctx.send(f"{self.sersifail} No entry found with ID {id_num}")
+            return
 
-        if not msg_sent:                          # creates webhook if none found
-            webhook = await dm_channel.create_webhook(name="dm webhook by sersi")
-            await webhook.send(message.content, username=message.author.display_name, avatar_url=message.author.display_avatar.url)
-            msg_sent = True
+        member = ctx.guild.get_member(int(member_snowflake))
+
+        if member is not None:
+            await ctx.send(f"Message with ID `{id_num}` was sent by {member.mention} ({member.id})")
+        else:
+            await ctx.send(f"Message with ID `{id_num}` was sent by `{member_snowflake}`, who is not found on this server")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        
+        if message.guild is None and message.author != self.bot.user:
+
+            if message.content.lower() == "secret":
+                self.active_secret_dms.append(message.author.id)
+                await message.author.send("The next DM will be secret! This is not an invitation to break the rules.")
+                return
+
+            elif message.author.id in self.active_secret_dms:
+                self.active_secret_dms.remove(message.author.id)
+                ID = str(uuid.uuid4())
+
+                secret = nextcord.Embed(
+                    title="Secret Message",
+                    description=message.content,
+                    colour=nextcord.Colour.blurple())
+                secret.set_footer(text=ID)
+
+                secretlist = {}
+                with open(self.filename, 'rb') as f:
+                    try:
+                        secretlist = pickle.load(f)
+                    except EOFError:
+                        pass
+
+                secretlist[ID] = message.author.id
+                print(f"Current secret dict: {secretlist}")
+
+                with open(self.filename, 'wb') as file:
+                    pickle.dump(secretlist, file)
+
+                dm_channel = self.bot.get_channel(get_config_int("CHANNELS", "secret"))
+                channel_webhooks = await dm_channel.webhooks()
+
+                for webhook in channel_webhooks:                  # tries to find existing webhook
+                    if webhook.name == "dm webhook by sersi":
+                        await webhook.send(embed=secret, username="Anonymous User")
+                        msg_sent = True
+
+                if not msg_sent:                          # creates webhook if none found
+                    webhook = await dm_channel.create_webhook(name="dm webhook by sersi")
+                    await webhook.send(embed=secret, username="Anonymous User")
+                    msg_sent = True
+
+            else:
+                if not get_config_bool("MSG", "forward dms", False):
+                    return
+
+                dm_channel = self.bot.get_channel(get_config_int("CHANNELS", "dm forward"))   # please name and config
+                channel_webhooks = await dm_channel.webhooks()
+                msg_sent = False
+
+                for webhook in channel_webhooks:                  # tries to find existing webhook
+                    if webhook.name == "dm webhook by sersi":
+                        await webhook.send(message.content, username=message.author.display_name, avatar_url=message.author.display_avatar.url)
+                        msg_sent = True
+
+                if not msg_sent:                          # creates webhook if none found
+                    webhook = await dm_channel.create_webhook(name="dm webhook by sersi")
+                    await webhook.send(message.content, username=message.author.display_name, avatar_url=message.author.display_avatar.url)
+                    msg_sent = True
 
 
 def setup(bot):
