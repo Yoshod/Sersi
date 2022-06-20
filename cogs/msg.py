36acdde243b44672425338f28415833f84acb5cd
Yoshod <stephenkyle1502@hotmail.co.uk>
2022-06-21 00:11:55 +0100

    Added slur detection for anon messages

--- cogs/msg.py
+++ cogs/msg.py
@@ -2,10 +2,12 @@ import nextcord
 import uuid
 import pickle
 from nextcord.ext import commands
+from nextcord.ui import Button, View
 
 from configutils import get_config, get_config_int, get_config_bool
-from permutils import permcheck, is_mod, is_senior_mod
+from permutils import permcheck, is_mod, is_senior_mod, cb_is_mod
 from encryptionutils import *
+from slurdetector import detectSlur
 
 
 class Messages(commands.Cog):
@@ -17,6 +19,66 @@ class Messages(commands.Cog):
         self.sersifail = get_config('EMOTES', 'fail')
         self.active_secret_dms = []
         self.filename = ("secret_dms.pkl")
+
+    async def cb_action_taken(self, interaction):
+        new_embed = interaction.message.embeds[0]
+        new_embed.add_field(name="Action Taken By", value=interaction.user.mention, inline=False)
+        new_embed.colour = nextcord.Colour.brand_green()
+        await interaction.message.edit(embed=new_embed, view=None)
+
+        # Logging
+        channel = self.bot.get_channel(get_config_int('CHANNELS', 'logging'))
+        embedLogVar = nextcord.Embed(
+            title="Action Taken Pressed",
+            description="Action has been taken by a moderator in response to a report.",
+            color=nextcord.Color.from_rgb(237, 91, 6))
+        embedLogVar.add_field(name="Report:", value=interaction.message.jump_url, inline=False)
+        embedLogVar.add_field(name="Moderator:", value=f"{interaction.user.mention} ({interaction.user.id})", inline=False)
+        await channel.send(embed=embedLogVar)
+
+    async def cb_acceptable_use(self, interaction):
+        new_embed = interaction.message.embeds[0]
+        new_embed.add_field(name="Usage Deemed Acceptable By", value=interaction.user.mention, inline=False)
+        new_embed.colour = nextcord.Colour.light_grey()
+        await interaction.message.edit(embed=new_embed, view=None)
+
+        # Logging
+        channel = self.bot.get_channel(get_config_int('CHANNELS', 'logging'))
+        embedLogVar = nextcord.Embed(
+            title="Acceptable Use Pressed",
+            description="Usage of a slur has been deemed acceptable by a moderator in response to a report.",
+            color=nextcord.Color.from_rgb(237, 91, 6))
+        embedLogVar.add_field(name="Report:", value=interaction.message.jump_url, inline=False)
+        embedLogVar.add_field(name="Moderator:", value=f"{interaction.user.mention} ({interaction.user.id})", inline=False)
+        await channel.send(embed=embedLogVar)
+
+    async def cb_false_positive(self, interaction):
+        new_embed = interaction.message.embeds[0]
+        new_embed.add_field(name="Deemed As False Positive By:", value=interaction.user.mention, inline=False)
+        new_embed.colour = nextcord.Colour.brand_red()
+        await interaction.message.edit(embed=new_embed, view=None)
+        channel = self.bot.get_channel(get_config_int('CHANNELS', 'false positives'))
+
+        embedVar = nextcord.Embed(
+            title="Marked as false positive",
+            color=nextcord.Color.from_rgb(237, 91, 6))
+
+        for field in new_embed.fields:
+            if field.name in ["Context:", "Slurs Found:"]:
+                embedVar.add_field(name=field.name, value=field.value, inline=False)
+
+        embedVar.add_field(name="Report URL:", value=interaction.message.jump_url, inline=False)
+        await channel.send(embed=embedVar)
+
+        # Logging
+        channel = self.bot.get_channel(get_config_int('CHANNELS', 'logging'))
+        embedLogVar = nextcord.Embed(
+            title="False Positive Pressed",
+            description="Detected slur has been deemed a false positive by a moderator in response to a report.",
+            color=nextcord.Color.from_rgb(237, 91, 6))
+        embedLogVar.add_field(name="Report:", value=interaction.message.jump_url, inline=False)
+        embedLogVar.add_field(name="Moderator:", value=f"{interaction.user.mention} ({interaction.user.id})", inline=False)
+        await channel.send(embed=embedLogVar)
 
     @commands.command()
     async def dm(self, ctx, recipient: nextcord.Member, *, message):
@@ -122,6 +184,41 @@ class Messages(commands.Cog):
                     if webhook.name == "dm webhook by sersi":
                         await webhook.send(embed=secret, username="Anonymous User")
                         msg_sent = True
+
+                detected_slurs = detectSlur(message.content)
+                if len(detected_slurs) > 0:  # checks slur heat
+                    channel = self.bot.get_channel(get_config_int('CHANNELS', 'alert'))
+                    slurembed = nextcord.Embed(
+                        title="Slur(s) Detected",
+                        description="A slur has been detected in an anonymous message. If moderator action is required you should deanonymise the message.",
+                        color=nextcord.Color.from_rgb(237, 91, 6)
+                    )
+                    slurembed.add_field(name="Message ID:", value=ID, inline=False)
+
+                    if len(message.content) < 1024:
+                        slurembed.add_field(name="Context:", value=message.content, inline=False)
+                    else:
+                        slurembed.add_field(name="Context:", value="`MESSAGE TOO LONG`", inline=False)
+
+                    slurembed.add_field(name="Slurs Found:", value=", ".join(set(detected_slurs)), inline=False)
+                    slurembed.set_footer(text="Sersi Slur Detection Alert")
+
+                    action_taken = Button(label="Action Taken")
+                    action_taken.callback = self.cb_action_taken
+
+                    acceptable_use = Button(label="Acceptable Use")
+                    acceptable_use.callback = self.cb_acceptable_use
+
+                    false_positive = Button(label="False Positive")
+                    false_positive.callback = self.cb_false_positive
+
+                    button_view = View(timeout=None)
+                    button_view.add_item(action_taken)
+                    button_view.add_item(acceptable_use)
+                    button_view.add_item(false_positive)
+                    button_view.interaction_check = cb_is_mod
+
+                    await channel.send(embed=slurembed, view=button_view)
 
                 if not msg_sent:                          # creates webhook if none found
                     webhook = await dm_channel.create_webhook(name="dm webhook by sersi")
