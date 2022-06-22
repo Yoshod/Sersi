b399701cdbc528560d69075f69a4e6d5d09ce6f2
Yoshod <stephenkyle1502@hotmail.co.uk>
2022-06-22 21:12:28 +0100

    pep8 compliance

--- cogs/about.py
+++ cogs/about.py
@@ -7,29 +7,29 @@ class About(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.verNum = "`3.2.0`"
+        self.version_num = "`3.2.0`"
 
         authors = []
         with open("Files/About/authors.txt", "r") as file:
             for line in file:
                 line = line.replace('\n', '')
                 authors.append(line)
-        self.authorsList = authors
+        self.authors_list = authors
 
     @commands.command()
     async def about(self, ctx):
         """Displays basic information about the bot."""
 
-        authorString = ""
-        for author in self.authorsList:
-            authorString = authorString + f"{author}\n"
+        author_string = ""
+        for author in self.authors_list:
+            author_string = author_string + f"{author}\n"
 
         about = nextcord.Embed(
             title="About Sersi",
             description="Sersi is the custom moderation help bot for Adam Something Central.",
             color=nextcord.Color.from_rgb(237, 91, 6))
-        about.add_field(name="Version:", value=self.verNum, inline=False)
-        about.add_field(name="Authors:", value=authorString, inline=False)
+        about.add_field(name="Version:", value=self.version_num, inline=False)
+        about.add_field(name="Authors:", value=author_string, inline=False)
         await ctx.send(embed=about)
 
 
