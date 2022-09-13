22b273487221861f15f57f3b6ffd4791a062ac18
Yoshod <stephenkyle1502@hotmail.co.uk>
2022-09-13 19:12:17 +0100

    [NEW] #80 Moderator Application Modal

--- cogs/moderators.py
+++ cogs/moderators.py
@@ -4,7 +4,7 @@ from nextcord.ui import Button, View, Modal
 
 from baseutils import ConfirmView, DualCustodyView
 from configutils import get_options, get_config, get_config_int
-from permutils import permcheck, is_staff, is_senior_mod
+from permutils import is_dark_mod, permcheck, is_staff, is_senior_mod
 
 
 class ModAppModal(Modal):
@@ -26,11 +26,6 @@ class ModAppModal(Modal):
         self.vc = nextcord.ui.TextInput(label="Are You Able To Voice Chat", min_length=2, max_length=1024, required=True)
         self.add_item(self.vc)
 
-
-class ModAppNextSteps(Modal):
-    def __init__(self, userID: int):
-        self.userID = userID
-
     async def callback(self, interaction):
         """Run whenever the 'submit' button is pressed."""
         applicant_id = interaction.user.id
@@ -39,26 +34,23 @@ class ModAppNextSteps(Modal):
             title="Moderator Application Sent",
             description=f"User {interaction.user.name} ({interaction.user.id})",
             color=nextcord.Color.from_rgb(237, 91, 6))
-        application_embed.add_field(name=self.aboutq.label,    value=self.aboutq.value,      inline=False)
-        application_embed.add_field(name=self.whymod.label,  value=self.whymod.value,    inline=False)
-        application_embed.add_field(name=self.priorexp.label, value=self.priorexp.value,   inline=False)
-        application_embed.add_field(name=self.age.label,   value=self.age.value,     inline=False)
-        application_embed.add_field(name=self.vc.label,   value=self.vc.value,     inline=False)
+        application_embed.add_field(name=self.aboutq.label,     value=self.aboutq.value,    inline=False)
+        application_embed.add_field(name=self.whymod.label,     value=self.whymod.value,    inline=False)
+        application_embed.add_field(name=self.priorexp.label,   value=self.priorexp.value,  inline=False)
+        application_embed.add_field(name=self.age.label,        value=self.age.value,       inline=False)
+        application_embed.add_field(name=self.vc.label,         value=self.vc.value,        inline=False)
 
-        async def cb_accept(interaction):
-            user = interaction.client.get_user(applicant_id)
-            accepted_embed = nextcord.Embed(
-                title="Moderator Application: Next Steps",
-                colour=nextcord.Colour.from_rgb(237, 91, 6),
-                description="Hey there!\n\nYour moderator application has been moved to the next stage. The next step is to arrange a time to talk over voice chat. A Dark Moderator will be in touch shortly.")
-            
+        accept_bttn = Button(custom_id=f"mod-application-next-steps:{applicant_id}", label="Move To Next Steps", style=nextcord.ButtonStyle.green)
+        reject_bttn = Button(custom_id=f"mod-application-reject:{applicant_id}", label="Reject Application", style=nextcord.ButtonStyle.red)
+        review_bttn = Button(custom_id=f"mod-application-review:{applicant_id}", label="Under Review", style=nextcord.ButtonStyle.grey)
 
-        async def cb_reject(interaction):
-            await interaction.response.send_modal(ModAppRejected(applicant_id))
+        button_view = View(auto_defer=False)
+        button_view.add_item(accept_bttn)
+        button_view.add_item(reject_bttn)
+        button_view.add_item(review_bttn)
 
-
-class ModAppRejected(Modal):
-    pass
+        channel = interaction.client.get_channel(get_config_int('CHANNELS', 'modapplications'))
+        await channel.send(embed=application_embed, view=button_view)
 
 
 class Moderators(commands.Cog):
@@ -372,17 +364,14 @@ class Moderators(commands.Cog):
 
     @commands.command()
     async def mod_apps(self, ctx):
-        if ctx.author.id in (261870562798731266, 348142492245426176):
-            pass
-
-        else:
+        if not await permcheck(ctx, is_dark_mod):
             return
 
         await ctx.message.delete()
 
         test_embed = nextcord.Embed(
             title="Moderator Application",
-            description="If this doesn't work I'll fucking kill myself.",
+            description="Press the button below to apply to become a moderator on Adam Something Central.",
             colour=nextcord.Color.from_rgb(237, 91, 6))
         open_modal = Button(label="Open Form", style=nextcord.ButtonStyle.blurple)
         open_modal.callback = self.cb_open_mod_modal
@@ -391,6 +380,79 @@ class Moderators(commands.Cog):
         button_view.add_item(open_modal)
 
         await ctx.send(embed=test_embed, view=button_view)
+
+    @commands.command()
+    async def mod_info(self, ctx):
+        if not await permcheck(ctx, is_dark_mod):
+            return
+
+        await ctx.message.delete()
+
+        info_embed = nextcord.Embed(
+            title="Moderator Application Information",
+            description="Moderator applications are open all year round! If you wish to be a moderator on Adam Something Central, here are somethings to be aware of:",
+            colour=nextcord.Color.from_rgb(237, 91, 6))
+        info_embed.add_field(name="Server Membership", value="We want all of our moderators to be members of the Adam Something Central community. This means if you have been here for less than two months we will not be able to consider you.", inline=False)
+        info_embed.add_field(name="Moderation History", value="Whilst having a moderation history on the server does not automatically prevent you from becoming a moderator, expect to be challenged on your moderation history.", inline=False)
+        info_embed.add_field(name="Age", value="Whilst we do not explicitly require moderators to be of a certain age, bar the fact they must be old enough to use discord, those under the age of 18 can expect a little more grilling in order to determine maturity.", inline=False)
+        info_embed.add_field(name="Applying", value="To apply all you have to do is press the 'Open Form' button above. This will show a short form inside of discord that you can fill in. The process is easy, and you will usually get a response within two days.", inline=False)
+        await ctx.send(embed=info_embed)
+
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction):
+        try:
+            id_name, id_extra = interaction.data["custom_id"].split(":", 1)
+        except ValueError:
+            id_name = interaction.data["custom_id"]
+            id_extra = None
+
+        match id_name:
+            case "mod-application-next-steps":
+                if await permcheck(interaction, is_dark_mod):
+                    user = interaction.guild.get_member(int(id_extra))
+
+                    updated_form = interaction.message.embeds[0]
+                    updated_form.add_field(name="Application Advanced by:", value=interaction.user.mention)
+                    await interaction.message.edit(embed=updated_form, view=None)
+
+                    advance_embed = nextcord.Embed(
+                        title="Your Moderator Application",
+                        description="Your moderator application has been advanced to the next steps. Please create a Dark Moderator on Adam Something Central.",
+                        colour=nextcord.Color.from_rgb(237, 91, 6))
+                    await user.send(embed=advance_embed)
+
+            case "mod-application-reject":
+                if await permcheck(interaction, is_dark_mod):
+                    user = interaction.guild.get_member(int(id_extra))
+
+                    updated_form = interaction.message.embeds[0]
+                    updated_form.add_field(name="Application Rejected by:", value=interaction.user.mention)
+                    await interaction.message.edit(embed=updated_form, view=None)
+
+                    rejection_embed = nextcord.Embed(
+                        title="Your Moderator Application",
+                        description="Your moderator application has been rejected. Thanks for applying, we encourage you to try again in the future.",
+                        colour=nextcord.Color.from_rgb(237, 91, 6))
+                    await user.send(embed=rejection_embed)
+
+            case "mod-application-review":
+                if await permcheck(interaction, is_dark_mod):
+                    user = interaction.guild.get_member(int(id_extra))
+
+                    updated_form = interaction.message.embeds[0]
+                    updated_form.add_field(name="Review notified by:", value=interaction.user.mention)
+                    accept_bttn = Button(custom_id=f"mod-application-next-steps:{id_extra}", label="Move To Next Steps", style=nextcord.ButtonStyle.green)
+                    reject_bttn = Button(custom_id=f"mod-application-reject:{id_extra}", label="Reject Application", style=nextcord.ButtonStyle.red)
+                    button_view = View(auto_defer=False)
+                    button_view.add_item(accept_bttn)
+                    button_view.add_item(reject_bttn)
+                    await interaction.message.edit(embed=updated_form, view=button_view)
+
+                    review_embed = nextcord.Embed(
+                        title="Your Moderator Application",
+                        description="Your moderator application has been received and is now under consideration. You will receive more information in the coming days.",
+                        colour=nextcord.Color.from_rgb(237, 91, 6))
+                    await user.send(embed=review_embed)
 
 
 def setup(bot):
