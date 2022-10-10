Yoshod/Sersi

commit 0e15de0dde496868541cf8cdb67f2164596980e4

[FIX] #117

ticketutils.py
@@ -188,9 +188,7 @@ async def ticket_close(config: configutils.Configuration, interaction, user, tic
 
         await output_channel.send(embed=log_embed, file=transcript_file)
 
-        transcript_file = nextcord.File(
-            io.BytesIO(transcript.encode()),
-            filename=f"transcript-{channel.name}.html",
-        )
-
-        await user.send(embed=log_embed, file=transcript_file)
+        try:
+            await user.send(embed=log_embed, file=transcript_file)
+        except nextcord.errors.Forbidden:
+            pass

commit ab5043b4824ae5f4d4541871162873ac35441673

[FIX] Squashed bug introduced in 0e15de0
The second transcript generation was removed in 0e15de0 which results in a transcript of NaN kb being sent.

ticketutils.py
@@ -187,6 +187,10 @@ async def ticket_close(config: configutils.Configuration, interaction, user, tic
             color=nextcord.Color.from_rgb(237, 91, 6))
 
         await output_channel.send(embed=log_embed, file=transcript_file)
+        transcript_file = nextcord.File(
+            io.BytesIO(transcript.encode()),
+            filename=f"transcript-{channel.name}.html",
+        )
 
         try:
             await user.send(embed=log_embed, file=transcript_file)