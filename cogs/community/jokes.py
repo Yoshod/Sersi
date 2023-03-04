29c654c54962a47d16ff0a12c393254e24d76ad6
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2023-03-04 22:57:40 +0100

    [UPD] update jokes

--- cogs/community/jokes.py
+++ cogs/community/jokes.py
@@ -8,7 +8,7 @@ from webhookutils import send_webhook_message
 
 
 class Jokes(commands.Cog):
-    def __init__(self, bot, config: Configuration):
+    def __init__(self, bot: commands.Bot, config: Configuration):
         self.bot = bot
         self.config = config
 
@@ -53,7 +53,7 @@ class Jokes(commands.Cog):
         return output_text
 
     @commands.command()
-    async def nevermod(self, ctx, member: nextcord.Member):
+    async def nevermod(self, ctx: commands.Context, member: nextcord.Member):
         if not is_mod(ctx.author):  # don't replace, it is funny
             await ctx.send(
                 "You're not a mod, you should not run this, right? ;)\n\nAnyways let's nevermod **you** instead as a "
@@ -74,7 +74,7 @@ class Jokes(commands.Cog):
         await ctx.send(embed=nevermod_embed)
 
     @commands.command()
-    async def uwu(self, ctx, *, message=""):
+    async def uwu(self, ctx: commands.Context, *, message: str=""):
         """OwO *nuzzles the command*.
 
         Takes message and uwuifies it.
@@ -93,7 +93,7 @@ class Jokes(commands.Cog):
         )
 
     @commands.command()
-    async def owo(self, ctx, *, message=""):
+    async def owo(self, ctx: commands.Context, *, message=""):
         """OwO *nuzzles the command*.
 
         Takes message and owoifies it.
@@ -114,7 +114,7 @@ class Jokes(commands.Cog):
         )
 
     @commands.command(aliases=["coin", "coinflip"])
-    async def flip(self, ctx):
+    async def flip(self, ctx: commands.Context):
         flip_result = random.randint(1, 2)
         if flip_result == 2:
             await ctx.reply(
@@ -127,7 +127,7 @@ class Jokes(commands.Cog):
 
     # events
     @commands.Cog.listener()
-    async def on_message(self, message):
+    async def on_message(self, message: nextcord.Message):
 
         if message.author.bot:  # ignores message if message is by bot
             return
@@ -135,7 +135,7 @@ class Jokes(commands.Cog):
         if message.guild is None:
             return
 
-        elif "admin furry stash" in message.content.lower():
+        if "admin furry stash" in message.content.lower():
             randomValue = random.randint(1, 10)
             if randomValue == 10:
                 embed = nextcord.Embed(
@@ -144,54 +144,71 @@ class Jokes(commands.Cog):
                     colour=nextcord.Colour.from_rgb(138, 43, 226),
                 )
                 await message.channel.send(embed=embed)
-            else:
                 return
 
-        elif (
+        if (
             "question of life the universe and everything"
             in message.content.lower().replace(",", "")
             or "ultimate question" in message.content.lower()
         ):
-            randomValue = random.randint(1, 5)
+            randomValue = random.randint(1, 3)
             if randomValue == 1:
                 await message.channel.send("The answer is ||42||")
-            else:
                 return
 
-        elif message.content.lower() == "literally 1984":
-            randomValue = random.randint(1, 10)
-            if randomValue >= 9:
+        if "literally 1984" in message.content.lower():
+            randomValue = random.randint(1, 4)
+            if randomValue == 1:
+                years = [1419, 1483, 1618, 1812, 1848, 1894, 1942, 1948, 1968, 1989]
                 await message.channel.send(
-                    "Oh my god, so true. It literally is like George Orlando's 1812"
+                    f"Oh my god, so true. It literally is like George Orlando's {random.choice(years)}"
                 )
-            else:
                 return
 
-        elif message.content.lower() == "nya":
-            randomValue = random.randint(1, 10)
-            if randomValue == 10:
+        if message.content.lower() == "nya":
+            randomValue = random.randint(1, 15)
+            if randomValue == 1:
                 await message.channel.send(
                     f"Nya... nya? What are you, a fucking weeb {message.author.mention}?"
                 )
-            else:
                 return
 
-        elif message.content.lower() == "meow":
-            randomValue = random.randint(1, 10)
-            if randomValue == 10:
+        if message.content.lower() == "meow":
+            randomValue = random.randint(1, 20)
+            if randomValue == 1:
                 await message.channel.send(
                     f"Meow meow meow, we get it you have a prissy attitude {message.author.mention}, we already noticed."
                 )
-            else:
+                return
+            
+        if message.content.lower() == "meow":
+            randomValue = random.randint(1, 20)
+            if randomValue == 1:
+                await message.channel.send(
+                    f"Meow meow meow, we get it you have a prissy attitude {message.author.mention}, we already noticed."
+                )
+                return
+            
+        if (
+            message.type == nextcord.MessageType.reply
+            and "is this" in message.content.lower()
+            and len(message.mentions) == 1
+        ):
+            randomValue = random.randint(1, 100)
+            if randomValue == 1:
+                await message.reply(
+                    f"No, this is {message.mentions[0].nick or message.mentions[0].name}."
+                )
                 return
 
-        elif message.author.is_on_mobile():
-            randomValue = random.randint(1, 200000)
-            if randomValue in [1, 2]:
+        if message.author.is_on_mobile():
+            randomValue = random.randint(1, 100000)
+            if randomValue == 1:
+                genders = ['man', 'woman', 'enby']
                 await message.reply(
-                    "Discord mobile was the greatest mistake in the history of mankind"
+                    f"Discord mobile was the greatest mistake in the history of {random.choice(genders)}kind"
                 )
-            elif randomValue in [3, 4, 5]:
+            elif randomValue < 4:
                 await message.reply("Phone user detected, opinion rejected")
 
 
