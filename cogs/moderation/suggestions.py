1303bf26e3c5ae256af0117275174c6d668372c2
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2023-03-11 16:05:30 +0100

    [NEW] new API OwO

--- cogs/moderation/suggestions.py
+++ cogs/moderation/suggestions.py
@@ -11,28 +11,28 @@ class Suggestions(commands.Cog):
         self.config = config
 
 
-@commands.command()
-async def suggest(self, ctx):
-    suggest_embed = nextcord.Embed(
-        title=f"New Suggestion by {ctx.author.mention}",
-        description="A new suggestion has been submitted, please review this properly before deciding on whether to make available for the wider server to vote upon.",
-        colour=nextcord.Color.from_rgb(237, 91, 6),
-    )
-    suggest_embed.add_field(
-        name="Suggester", value=f"{ctx.author.mention} ({ctx.author.id})"
-    )
-    suggest_embed.add_field(name="Suggestion", value=ctx.message.content)
+    @commands.command()
+    async def suggest(self, ctx):
+        suggest_embed = nextcord.Embed(
+            title=f"New Suggestion by {ctx.author.mention}",
+            description="A new suggestion has been submitted, please review this properly before deciding on whether to make available for the wider server to vote upon.",
+            colour=nextcord.Color.from_rgb(237, 91, 6),
+        )
+        suggest_embed.add_field(
+            name="Suggester", value=f"{ctx.author.mention} ({ctx.author.id})"
+        )
+        suggest_embed.add_field(name="Suggestion", value=ctx.message.content)
 
-    yes = Button(label="Approve", style=nextcord.ButtonStyle.green)
-    yes.callback = self.cb_rq_yes
+        yes = Button(label="Approve", style=nextcord.ButtonStyle.green)
+        yes.callback = self.cb_rq_yes
 
-    no = Button(label="Deny", style=nextcord.ButtonStyle.red)
-    no.callback = self.cb_no
+        no = Button(label="Deny", style=nextcord.ButtonStyle.red)
+        no.callback = self.cb_no
 
-    button_view = View(timeout=None)
-    button_view.add_item(yes)
-    button_view.add_item(no)
-    button_view.interaction_check = cb_is_cet
+        button_view = View(timeout=None)
+        button_view.add_item(yes)
+        button_view.add_item(no)
+        button_view.interaction_check = cb_is_cet
 
 
 def setup(bot, **kwargs):
