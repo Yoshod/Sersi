4ffa79971afa8a17ad05ff6dc8c3c97be23c73b8
Yoshod <stephenkyle1511@icloud.com>
2023-04-17 00:53:31 +0100

    [NEW] Get Case By ID

--- caseutils.py
+++ caseutils.py
@@ -93,3 +93,102 @@ def ping_case(
 
     conn.commit()
     conn.close()
+
+
+def get_case_by_id(
+    config: Configuration,
+    case_id: str,
+) -> dict:
+    conn = sqlite3.connect(config.datafiles.sersi_db)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM cases WHERE id=?", (case_id))
+
+    try:
+        row = cursor.fetchone()
+
+    except TypeError:
+        cursor.close()
+        return "No Case"
+
+    case_type = row[1]
+
+    match (case_type):
+        case "Slur Usage":
+            cursor.execute("SELECT * FROM slur_cases WHERE id=?", (case_id))
+
+            try:
+                row = cursor.fetchone()
+
+            except TypeError:
+                cursor.close()
+                return "Exists Not Found"
+
+            return {
+                "ID": f"{row[0]}",
+                "Slur Used": f"{row[1]}",
+                "Report URL": f"{row[2]}",
+                "Offender ID": row[3],
+                "Moderator ID": row[4],
+                "Timestamp": row[5],
+            }
+
+    match (case_type):
+        case "Reformation":
+            cursor.execute("SELECT * FROM reformation_cases WHERE id=?", (case_id))
+
+            try:
+                row = cursor.fetchone()
+
+            except TypeError:
+                cursor.close()
+                return "Exists Not Found"
+
+            return {
+                "ID": f"{row[0]}",
+                "Case Number": row[1],
+                "Offender ID": row[2],
+                "Moderator ID": row[3],
+                "Channel ID": row[4],
+                "Reason": f"{row[5]}",
+                "Timestamp": row[6],
+            }
+
+    match (case_type):
+        case "Probation":
+            cursor.execute("SELECT * FROM probation_cases WHERE id=?", (case_id))
+
+            try:
+                row = cursor.fetchone()
+
+            except TypeError:
+                cursor.close()
+                return "Exists Not Found"
+
+            return {
+                "ID": f"{row[0]}",
+                "Offender ID": row[1],
+                "Initial Moderator ID": row[2],
+                "Approving Moderator ID": row[3],
+                "Reason": f"{row[4]}",
+                "Timestamp": row[5],
+            }
+
+    match (case_type):
+        case "Bad Faith Ping":
+            cursor.execute("SELECT * FROM bad_faith_ping_cases WHERE id=?", (case_id))
+
+            try:
+                row = cursor.fetchone()
+
+            except TypeError:
+                cursor.close()
+                return "Exists Not Found"
+
+            return {
+                "ID": f"{row[0]}",
+                "Report URL": f"{row[1]}",
+                "Offender ID": row[2],
+                "Moderator ID": row[3],
+                "Timestamp": row[4],
+            }
