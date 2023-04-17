ea9cbd37e517b54a7f72a3ff3154390ea8316f5f
Yoshod <stephenkyle1511@icloud.com>
2023-04-17 01:57:48 +0100

    [WIP] Cases Rewrite slur cases working

--- cogs/moderation/cases.py
+++ cogs/moderation/cases.py
@@ -1,9 +1,8 @@
 import nextcord
-import pickle
 from nextcord.ext import commands
 
 from baseutils import PageView, SersiEmbed
-from caseutils import get_member_cases
+from caseutils import get_case_by_id
 from configutils import Configuration
 from permutils import permcheck, is_mod
 
@@ -12,144 +11,83 @@ class Cases(commands.Cog):
     def __init__(self, bot: commands.Bot, config: Configuration):
         self.bot = bot
         self.config = config
-        self.sersifail = config.emotes.fail
-        self.case_history_file = config.datafiles.casehistory
-        self.case_details_file = config.datafiles.casedetails
-        self.case_history = {}
-        self.case_details = {}
 
-    @commands.command(aliases=["c", "usercases", "modcases"])
-    async def cases(self, ctx: commands.Context, search_term):
-        if not await permcheck(ctx, is_mod):
+    @nextcord.slash_command(
+        dm_permission=False,
+        guild_ids=[977377117895536640, 856262303795380224],
+        description="Used to get a case",
+    )
+    async def get_case(
+        self,
+        interaction: nextcord.Interaction,
+        case_id: str = nextcord.SlashOption(
+            name="case_id",
+            description="Case ID",
+            min_length=22,
+            max_length=22,
+        ),
+    ):
+        if not await permcheck(interaction, is_mod):
             return
 
-        member = await ctx.guild.get_member(search_term)
+        await interaction.response.defer(ephemeral=False)
 
-        if member:
-            cases_embed = SersiEmbed(
-                title=(f"{member.name}'s Cases"),
-                colour=nextcord.Color.from_rgb(237, 91, 6),
-            )
-            cases_embed.set_thumbnail(url=member.display_avatar.url)
+        sersi_case = get_case_by_id(self.config, case_id)
 
-            view = PageView(
-                config=self.config,
-                base_embed=cases_embed,
-                fetch_function=get_member_cases,
-                author=ctx.author,
-                entry_form="**__Case `{entry[0]}`__**: {entry[1]} <t:{entry[2]}>",
-                member_id=member.id,
-            )
-            await view.send_embed(ctx.channel)
-            with open(self.case_history_file, "rb") as file:
-                self.case_history = pickle.load(file)
+        match (sersi_case["Case Type"]):
+            case "Slur Usage":
+                case_embed = SersiEmbed()
+                case_embed.add_field(
+                    name="Case:", value=f"`{sersi_case['ID']}`", inline=True
+                )
+                case_embed.add_field(name="Type:", value="`Slur Usage`", inline=True)
 
-        elif not member:
-            with open(self.case_details_file, "rb") as file:
-                self.case_details = pickle.load(file)
+                moderator = interaction.guild.get_member(sersi_case["Moderator ID"])
 
-            if search_term not in self.case_details:
-                await ctx.send(f"{self.sersifail} Case {search_term} not found.")
+                if not moderator:
+                    case_embed.add_field(
+                        name="Moderator:",
+                        value=f"`{sersi_case['Moderator ID']}`",
+                        inline=True,
+                    )
 
-            elif self.case_details[search_term][0] == "Reformation":
-                user = ctx.guild.get_member(self.case_details[search_term][2])
-                moderator = ctx.guild.get_member(self.case_details[search_term][3])
-                reform_channel = ctx.guild.get_channel(
-                    self.case_details[search_term][4]
+                else:
+                    case_embed.add_field(
+                        name="Moderator:", value=f"{moderator.mention}", inline=True
+                    )
+
+                offender = interaction.guild.get_member(sersi_case["Offender ID"])
+
+                if not offender:
+                    case_embed.add_field(
+                        name="Offender:",
+                        value=f"`{sersi_case['Offender ID']}`",
+                        inline=True,
+                    )
+
+                else:
+                    case_embed.add_field(
+                        name="Offender:", value=f"{offender.mention}", inline=True
+                    )
+                    case_embed.set_thumbnail(url=offender.display_avatar.url)
+
+                case_embed.add_field(
+                    name="Slur Used:", value=sersi_case["Slur Used"], inline=False
                 )
 
-                case_embed = SersiEmbed(
-                    title=f"__**Reformation Case {search_term}**__",
-                    fields={
-                        "User:": f"{user.mention} ({user.id})",
-                        "Moderator:": f"{moderator.mention} ({moderator.id})",
-                        "Reformation Case Number:": self.case_details[search_term][1],
-                        "Reformation Channel:": f"{reform_channel.mention} ({reform_channel.id})",
-                        "Reason:": self.case_details[search_term][5],
-                        "Timestamp:": f"<t:{self.case_details[search_term][5]}:R>",
-                    },
-                )
-                case_embed.set_thumbnail(url=user.display_avatar.url)
-
-                await ctx.send(embed=case_embed)
-
-            elif self.case_details[search_term][0] == "Probation":
-                user = ctx.guild.get_member(self.case_details[search_term][1])
-                initial_moderator = ctx.guild.get_member(
-                    self.case_details[search_term][2]
-                )
-                approval_moderator = ctx.guild.get_member(
-                    self.case_details[search_term][3]
+                case_embed.add_field(
+                    name="Report URL:", value=sersi_case["Report URL"], inline=False
                 )
 
-                case_embed = SersiEmbed(
-                    title=f"__**Probation Case {search_term}**__",
-                    fields={
-                        "User:": f"{user.mention} ({user.id})",
-                        "Initial Moderator:": f"{initial_moderator.mention} ({initial_moderator.id})",
-                        "Approving Moderator:": f"{approval_moderator.mention} ({approval_moderator.id})",
-                        "Reason:": self.case_details[search_term][4],
-                        "Timestamp:": f"<t:{self.case_details[search_term][5]}:R>",
-                    },
+                case_embed.add_field(
+                    name="Timestamp:",
+                    value=(f"<t:{sersi_case['Timestamp']}:R>"),
+                    inline=True,
                 )
 
-                case_embed.set_thumbnail(url=user.display_avatar.url)
-                await ctx.send(embed=case_embed)
+                case_embed.set_footer(text="Sersi Case Tracking")
 
-            elif self.case_details[search_term][0] == "Anonymous Message Mute":
-                user = ctx.guild.get_member(self.case_details[search_term][1])
-                moderator = ctx.guild.get_member(self.case_details[search_term][2])
-
-                case_embed = SersiEmbed(
-                    title=f"__**Anonymous Message Mute Case {search_term}**__",
-                    fields={
-                        "User:": f"{user.mention} ({user.id})",
-                        "Moderator:": f"{moderator.mention} ({moderator.id})",
-                        "Reason:": self.case_details[search_term][3],
-                        "Timestamp:": f"<t:{self.case_details[search_term][4]}:R>",
-                    },
-                )
-
-                case_embed.set_thumbnail(url=user.display_avatar.url)
-                await ctx.send(embed=case_embed)
-
-            elif self.case_details[search_term][0] == "Slur Usage":
-                user = ctx.guild.get_member(self.case_details[search_term][3])
-                moderator = ctx.guild.get_member(self.case_details[search_term][4])
-
-                case_embed = SersiEmbed(
-                    title=f"__**Slur Usage Case {search_term}**__",
-                    fields={
-                        "User:": f"{user.mention} ({user.id})",
-                        "Moderator:": f"{moderator.mention} ({moderator.id})",
-                        "Slur Used:": self.case_details[search_term][1],
-                        "Report URL:": self.case_details[search_term][2],
-                        "Timestamp:": f"<t:{self.case_details[search_term][5]}:R>",
-                    },
-                )
-
-                case_embed.set_thumbnail(url=user.display_avatar.url)
-                await ctx.send(embed=case_embed)
-
-            elif self.case_details[search_term][0] == "Bad Faith Ping":
-                user = ctx.guild.get_member(self.case_details[search_term][1])
-                moderator = ctx.guild.get_member(self.case_details[search_term][3])
-
-                case_embed = SersiEmbed(
-                    title=f"__**Bad Faith Ping Case {search_term}**__",
-                    fields={
-                        "User:": f"{user.mention} ({user.id})",
-                        "Moderator:": f"{moderator.mention} ({moderator.id})",
-                        "Report URL:": self.case_details[search_term][2],
-                        "Timestamp:": f"<t:{self.case_details[search_term][4]}:R>",
-                    },
-                )
-
-                case_embed.set_thumbnail(url=user.display_avatar.url)
-                await ctx.send(embed=case_embed)
-
-            else:
-                return
+        await interaction.followup.send(embed=case_embed)
 
 
 def setup(bot, **kwargs):
