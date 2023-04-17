92471ad15d82f309665de1071526f2da5b79aefb
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2023-04-17 21:10:40 +0200

    [OPT] overworking modping.py
    basically just refactoring poorly named variables

--- cogs/moderation/modping.py
+++ cogs/moderation/modping.py
@@ -9,7 +9,7 @@ import logutils
 from baseutils import modmention_check, SersiEmbed
 from permutils import cb_is_mod
 from configutils import Configuration
-from caseutils import case_history, bad_faith_ping_case
+# from caseutils import case_history, bad_faith_ping_case
 
 
 class ModPing(commands.Cog):
@@ -17,7 +17,7 @@ class ModPing(commands.Cog):
         self.bot = bot
         self.config = config
 
-    async def cb_action_taken(self, interaction):
+    async def cb_action_taken(self, interaction: nextcord.Interaction):
         new_embed = interaction.message.embeds[0]
         new_embed.add_field(
             name="Action Taken By", value=interaction.user.mention, inline=True
@@ -26,78 +26,48 @@ class ModPing(commands.Cog):
         await interaction.message.edit(embed=new_embed, view=None)
 
         # Logging
-        embedLogVar = SersiEmbed(
+        logging_embed = SersiEmbed(
             title="Action Taken Pressed",
             description="Action has been taken by a moderator in response to a report.",
             fields={
                 "Report:": interaction.message.jump_url,
                 "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
             },
+            footer="Sersi Moderator Ping Detection",
         )
 
         channel = self.bot.get_channel(self.config.channels.logging)
-        await channel.send(embed=embedLogVar)
+        await channel.send(embed=logging_embed)
 
         await logutils.update_response(
             self.config, interaction.message, datetime.now(timezone.utc)
         )
 
-        """with open("files/Alerts/alerts.pkl", "rb") as previous_alerts:
-            previous_data = pickle.load(previous_alerts)
-
-        payload = [
-            "Mod Ping",
-            previous_data[str(interaction.message.created_at.timestamp())][1],
-            datetime.now().timestamp(),
-        ]
-
-        previous_data.update({str(interaction.message.created_at.timestamp()): payload})
-
-        with open("files/Alerts/alerts.pkl", "wb") as previous_alerts:
-            pickle.dump(
-                previous_data, previous_alerts, protocol=pickle.HIGHEST_PROTOCOL
-            )"""
-
-    async def cb_action_not_neccesary(self, interaction):
+    async def cb_action_not_necessary(self, interaction: nextcord.Interaction):
         new_embed = interaction.message.embeds[0]
         new_embed.add_field(
-            name="Action Not Neccesary", value=interaction.user.mention, inline=True
+            name="Action Not Necessary", value=interaction.user.mention, inline=True
         )
         new_embed.colour = nextcord.Colour.light_grey()
         await interaction.message.edit(embed=new_embed, view=None)
 
         # Logging
-        embedLogVar = SersiEmbed(
+        logging_embed = SersiEmbed(
             title="Action Not Necessary Pressed",
             description="A Moderator has deemed that no action is needed in response to a report.",
             fields={
                 "Report:": interaction.message.jump_url,
                 "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
             },
+            footer="Sersi Moderator Ping Detection",
         )
 
         channel = self.bot.get_channel(self.config.channels.logging)
-        await channel.send(embed=embedLogVar)
+        await channel.send(embed=logging_embed)
 
         await logutils.update_response(
             self.config, interaction.message, datetime.now(timezone.utc)
         )
-
-        """with open("files/Alerts/alerts.pkl", "rb") as previous_alerts:
-            previous_data = pickle.load(previous_alerts)
-
-        payload = [
-            "Mod Ping",
-            previous_data[str(interaction.message.created_at.timestamp())][1],
-            datetime.now().timestamp(),
-        ]
-
-        previous_data.update({str(interaction.message.created_at.timestamp()): payload})
-
-        with open("files/Alerts/alerts.pkl", "wb") as previous_alerts:
-            pickle.dump(
-                previous_data, previous_alerts, protocol=pickle.HIGHEST_PROTOCOL
-            )"""
 
     async def cb_bad_faith_ping(self, interaction):
         new_embed = interaction.message.embeds[0]
@@ -108,17 +78,18 @@ class ModPing(commands.Cog):
         await interaction.message.edit(embed=new_embed, view=None)
 
         # Logging
-        embedLogVar = SersiEmbed(
+        logging_embed = SersiEmbed(
             title="Bad Faith Ping Pressed",
             description="A moderation ping has been deemed bad faith by a moderator in response to a report.",
             fields={
                 "Report:": interaction.message.jump_url,
                 "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
             },
+            footer="Sersi Moderator Ping Detection",
         )
 
         channel = self.bot.get_channel(self.config.channels.logging)
-        await channel.send(embed=embedLogVar)
+        await channel.send(embed=logging_embed)
 
         case_data = []
         for field in new_embed.fields:
@@ -127,36 +98,19 @@ class ModPing(commands.Cog):
 
         member = interaction.guild.get_member(case_data[0])
 
-        unique_id = case_history(self.config, member.id, "Bad Faith Ping")
+        """unique_id = case_history(self.config, member.id, "Bad Faith Ping")
         bad_faith_ping_case(
             self.config,
             unique_id,
             interaction.message.jump_url,
             member.id,
             interaction.user.id,
-        )
+        )"""
 
         await logutils.update_response(
             self.config, interaction.message, datetime.now(timezone.utc)
         )
 
-        """with open("files/Alerts/alerts.pkl", "rb") as previous_alerts:
-            previous_data = pickle.load(previous_alerts)
-
-        payload = [
-            "Mod Ping",
-            previous_data[str(interaction.message.created_at.timestamp())][1],
-            datetime.now().timestamp(),
-        ]
-
-        previous_data.update({str(interaction.message.created_at.timestamp()): payload})
-
-        with open("files/Alerts/alerts.pkl", "wb") as previous_alerts:
-            pickle.dump(
-                previous_data, previous_alerts, protocol=pickle.HIGHEST_PROTOCOL
-            )"""
-
-    # events
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.guild is not None:
@@ -174,24 +128,24 @@ class ModPing(commands.Cog):
             "The Mod Zone",
             "The CET Zone",
             "Moderation Support",
-        ]:  # ignores certain channels on ASC, given by Juniper
+        ]:
             return
 
         if modmention_check(self.config, message.content):
             # Reply to user
-            embedVar = SersiEmbed(
+            response_embed: nextcord.Embed = SersiEmbed(
                 title="Moderator Ping Acknowledgment",
                 description=f"{message.author.mention} moderators have been notified of your ping and will investigate when able to do so.",
-                footer="Sersi Ping Detection Alert",
+                footer="Sersi Moderator Ping Detection",
             )
-            await message.channel.send(embed=embedVar)
+            await message.channel.send(embed=response_embed)
             await message.channel.send(
                 f"<@&{self.config.permission_roles.trial_moderator}>", delete_after=1
             )
 
             # notification for mods
             channel = self.bot.get_channel(self.config.channels.alert)
-            embedVar = SersiEmbed(
+            alert_embed: nextcord.Embed = SersiEmbed(
                 title="Moderator Ping",
                 description="A moderation role has been pinged, please investigate the ping and take action as appropriate.",
                 fields={
@@ -200,98 +154,75 @@ class ModPing(commands.Cog):
                     "Context:": message.content,
                     "URL:": message.jump_url,
                 },
-                footer="Sersi Ping Detection Alert",
+                footer="Sersi Moderator Ping Detection",
             )
 
             action_taken = Button(label="Action Taken")
             action_taken.callback = self.cb_action_taken
 
-            action_not_neccesary = Button(label="Action Not Neccesary")
-            action_not_neccesary.callback = self.cb_action_not_neccesary
+            action_not_necessary = Button(label="Action Not Necessary")
+            action_not_necessary.callback = self.cb_action_not_necessary
 
             bad_faith_ping = Button(label="Bad Faith Ping")
             bad_faith_ping.callback = self.cb_bad_faith_ping
 
             button_view = View(timeout=None)
             button_view.add_item(action_taken)
-            button_view.add_item(action_not_neccesary)
+            button_view.add_item(action_not_necessary)
             button_view.add_item(bad_faith_ping)
             button_view.interaction_check = cb_is_mod
 
-            alert = await channel.send(embed=embedVar, view=button_view)
+            alert = await channel.send(embed=alert_embed, view=button_view)
 
             await logutils.create_alert_log(
                 self.config, alert, logutils.AlertType.Ping, alert.created_at
             )
 
-            """try:
-                with open("files/Alerts/alerts.pkl", "rb") as previous_alerts:
-                    previous_data = pickle.load(previous_alerts)
-
-            except EOFError:
-                previous_data = {}
-
-            if alert.edited_at is None:
-                payload = ["Mod Ping", alert.created_at.timestamp()]
-
-            else:
-                payload = ["Mod Ping", alert.edited_at.timestamp()]
-
-            previous_data.update({str(payload[1]): payload})
-
-            with open("files/Alerts/alerts.pkl", "wb") as previous_alerts:
-                pickle.dump(
-                    previous_data, previous_alerts, protocol=pickle.HIGHEST_PROTOCOL
-                )"""
-
             await asyncio.sleep(10800)  # 3 hours
             updated_message = await alert.channel.fetch_message(alert.id)
-            if (
-                len(updated_message.embeds[0].fields) < 5
-            ):  # If there are less than 5 fields that means there is no field for response
+            # If there are less than 5 fields that means there is no field for response
+            if len(updated_message.embeds[0].fields) < 5:
                 await alert.reply(
                     content=f"<@&{self.config.permission_roles.moderator}> This alert has not had a recorded response."
                 )
-
-        # elif "<@809891646606409779>" in message.content:   # adam something ping
 
         elif (
             adam_something is not None
             and adam_something.mentioned_in(message)
             and not message.mention_everyone
         ):  # adam something ping
-            # notification for mods
             channel = self.bot.get_channel(self.config.channels.alert)
-            embedVar = SersiEmbed(
-                title="Moderator Ping",
-                description="A moderation role has been pinged, please investigate the ping and take action as appropriate.",
+            alert_embed: nextcord.Embed = SersiEmbed(
+                title="Adam Something Ping",
+                description="Some pleb just thought they were good enough to ping our benevolent server owner, "
+                "Adam Something.",
                 fields={
                     "Channel:": message.channel.mention,
                     "User:": message.author.mention,
                     "Context:": message.content,
                     "URL:": message.jump_url,
                 },
-                footer="Sersi Ping Detection Alert",
+                footer="Sersi Moderator Ping Detection",
             )
 
             action_taken = Button(label="Action Taken")
             action_taken.callback = self.cb_action_taken
 
-            action_not_neccesary = Button(label="Action Not Neccesary")
-            action_not_neccesary.callback = self.cb_action_not_neccesary
+            action_not_necessary = Button(label="Action Not Necessary")
+            action_not_necessary.callback = self.cb_action_not_necessary
 
             button_view = View(timeout=None)
             button_view.add_item(action_taken)
-            button_view.add_item(action_not_neccesary)
+            button_view.add_item(action_not_necessary)
             button_view.interaction_check = cb_is_mod
 
-            alert = await channel.send(embed=embedVar, view=button_view)
+            alert = await channel.send(embed=alert_embed, view=button_view)
 
             await asyncio.sleep(10800)  # 3 hours
             updated_message = await alert.channel.fetch_message(alert.id)
-            if (
-                len(updated_message.embeds[0].fields) < 5
-            ):  # If there are less than 5 fields that means there is no field for response
+
+            # If there are less than 5 fields that means there is no field for response
+            if len(updated_message.embeds[0].fields) < 5:
                 await alert.reply(
                     content=f"<@&{self.config.permission_roles.moderator}> This alert has not had a recorded response."
                 )
