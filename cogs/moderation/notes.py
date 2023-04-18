0f6274faa29e251384c955e0eb9766729680aece
Yoshod <stephenkyle1511@icloud.com>
2023-04-18 22:20:05 +0100

    [ADD] Get note by ID

--- cogs/moderation/notes.py
+++ cogs/moderation/notes.py
@@ -3,9 +3,7 @@ import time
 from nextcord.ext import commands
 
 from baseutils import SersiEmbed, PageView
-from noteutils import (
-    create_note,
-)
+from noteutils import create_note, get_note_by_id, fetch_notes_by_partial_id
 from configutils import Configuration
 from permutils import permcheck, is_mod, is_senior_mod
 
@@ -20,10 +18,10 @@ class Notes(commands.Cog):
         guild_ids=[977377117895536640, 856262303795380224],
         description="Note base command",
     )
-    async def note(self, interaction: nextcord.Interaction):
+    async def notes(self, interaction: nextcord.Interaction):
         pass
 
-    @note.subcommand(description="Add a note to a user")
+    @notes.subcommand(description="Add a note to a user")
     async def add(
         self,
         interaction: nextcord.Interaction,
@@ -45,6 +43,76 @@ class Notes(commands.Cog):
         await interaction.followup.send(
             f"{self.config.emotes.success} The note on {noted.mention} has been successfully created."
         )
+
+    @notes.subcommand(description="Used to get a note by its ID")
+    async def by_id(
+        self,
+        interaction: nextcord.Interaction,
+        note_id: str = nextcord.SlashOption(
+            name="note_id",
+            description="Note ID",
+            min_length=22,
+            max_length=22,
+        ),
+    ):
+        if not await permcheck(interaction, is_mod):
+            return
+
+        await interaction.response.defer(ephemeral=False)
+
+        sersi_note = get_note_by_id(self.config, note_id)
+
+        note_embed = SersiEmbed()
+        note_embed.add_field(
+            name="Note ID:", value=f"`{sersi_note['ID']}`", inline=True
+        )
+
+        moderator = interaction.guild.get_member(sersi_note["Noter ID"])
+
+        if not moderator:
+            note_embed.add_field(
+                name="Moderator:",
+                value=f"`{sersi_note['Noter ID']}`",
+                inline=True,
+            )
+
+        else:
+            note_embed.add_field(
+                name="Moderator:", value=f"{moderator.mention}", inline=True
+            )
+
+        noted = interaction.guild.get_member(sersi_note["Noted ID"])
+
+        if not noted:
+            note_embed.add_field(
+                name="User:",
+                value=f"`{sersi_note['Noted ID']}`",
+                inline=True,
+            )
+
+        else:
+            note_embed.add_field(name="User:", value=f"{noted.mention}", inline=True)
+            note_embed.set_thumbnail(url=noted.display_avatar.url)
+
+        note_embed.add_field(name="Note:", value=sersi_note["Note"], inline=False)
+
+        note_embed.add_field(
+            name="Timestamp:",
+            value=(f"<t:{sersi_note['Timestamp']}:R>"),
+            inline=True,
+        )
+
+        note_embed.set_footer(text="Sersi Notes")
+
+        await interaction.followup.send(embed=note_embed)
+
+    @by_id.on_autocomplete("note_id")
+    async def notes_by_id(self, interaction: nextcord.Interaction, note: str):
+        if not is_mod(interaction.user):
+            await interaction.response.send_autocomplete([])
+
+        notes = fetch_notes_by_partial_id(self.config, note)
+        await interaction.response.send_autocomplete(notes)
 
 
 def setup(bot, **kwargs):
