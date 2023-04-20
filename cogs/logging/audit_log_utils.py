21c558bd2c6cf7c7676cc4d1323e565d110b5e56
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2023-04-20 19:06:59 +0200

    [TEMP] raw audit log output

---
+++ cogs/logging/audit_log_utils.py
@@ -0,0 +1,24 @@
+import nextcord
+from nextcord.ext import commands
+
+from baseutils import SersiEmbed
+from configutils import Configuration
+
+
+class AuditLogs(commands.Cog):
+    def __init__(self, bot, config: Configuration):
+        self.bot = bot
+        self.config = config
+
+    @nextcord.slash_command(
+        dm_permission=False, guild_ids=[977377117895536640, 856262303795380224]
+    )
+    async def get_audit_logs(self, interaction: nextcord.Interaction):
+        message: str = ""
+        async for entry in interaction.guild.audit_logs(limit=10):
+            message += f"{entry.user} did {entry.action.name} to {entry.target}\n"
+        await interaction.send(message)
+
+
+def setup(bot, **kwargs):
+    bot.add_cog(AuditLogs(bot, kwargs["config"]))
