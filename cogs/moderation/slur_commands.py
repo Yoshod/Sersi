188040a7920fc42b02e242c3eb94819afb374e50
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2023-09-03 18:26:57 +0200

    [NEW] #139 slur detection slash commands

--- cogs/moderation/slur_commands.py
+++ cogs/moderation/slur_commands.py
@@ -22,15 +22,30 @@ class SlurCommands(nextcord.ext.commands.Cog):
         self.bot = bot
         self.config = config
 
-    @commands.command(aliases=["addsl"])
-    async def addslur(self, ctx, *, slur=""):
-        """Add a new slur to the list of slurs."""
-        if not await permcheck(ctx, is_mod):
-            return
+    @nextcord.slash_command(
+        dm_permission=False,
+        guild_ids=[977377117895536640, 856262303795380224],
+    )
+    async def slur_detection(self, interaction: nextcord.Interaction):
+        pass
 
-        elif slur == "":
-            await ctx.send(f"{ctx.author.mention} please provide a word to blacklist.")
+    @slur_detection.subcommand(
+        description="Add a new slur to the list of slurs.",
+    )
+    async def add_slur(
+        self,
+        interaction: nextcord.Interaction,
+        slur: str = nextcord.SlashOption(
+            description="The slur to add to the list of slurs.",
+            required=True,
+        ),
+    ):
+        """Add a new slur to the list of slurs."""
+        if not await permcheck(interaction, is_mod):
             return
+        
+        await interaction.response.defer(ephemeral=False)
+
         slur = "".join(slur)
         slur = clear_string(slur)
 
@@ -38,55 +53,58 @@ class SlurCommands(nextcord.ext.commands.Cog):
         for s in get_slurs_leet():
             if s in slur:
                 existing_slur = s
-
+        
         if existing_slur is not None:
-            await ctx.send(
+            await interaction.followup.send(
                 f"{self.config.emotes.fail} `{slur}` is in conflict with existing slur `{existing_slur}`; "
                 "cannot be added."
             )
             return
-
-        if slur in get_slurs_leet():
-            await ctx.send(
-                f"{self.config.emotes.fail} `{slur}` is already on the list of slurs"
-            )
-            return
-
-        await ctx.send(f"Slur to be added: {slur}")
+        
+        await interaction.followup.send(f"Slur to be added: {slur}")
         with open(self.config.datafiles.slurfile, "a") as file:
             file.write(slur)
             file.write("\n")
         load_slurs()  # reloads updated list into memory
 
         # logging
+
         await self.bot.get_channel(self.config.channels.logging).send(
             embed=SersiEmbed(
                 title="Slur Added",
                 description="A new slur has been added to the filter.",
                 fields={
-                    "Added By:": f"{ctx.message.author.mention} ({ctx.message.author.id})",
+                    "Added By:": f"{interaction.user.mention} ({interaction.user.id})",
                     "Slur Added:": slur,
                 },
             )
         )
 
-        await ctx.send(
+        await interaction.followup.send(
             f"{self.config.emotes.success} Slur added. Detection will start now."
         )
 
-    @commands.command(aliases=["addgw"])
-    async def addgoodword(self, ctx, *, word=""):
+    @slur_detection.subcommand(
+        description="Add a new goodword into the whitelist.",
+    )
+    async def add_goodword(
+        self,
+        interaction: nextcord.Interaction,
+        word: str = nextcord.SlashOption(
+            description="The word to whitelist.",
+            required=True,
+        ),
+    ):
         """Add a new goodword into the whitelist."""
-        if not await permcheck(ctx, is_mod):
+        if not await permcheck(interaction, is_mod):
             return
 
-        elif word == "":
-            await ctx.send(f"{ctx.author.mention} please provide a word to whitelist.")
-            return
+        await interaction.response.defer(ephemeral=False)
+
         word = "".join(word)
         word = clear_string(word)
         if word in get_goodwords():
-            await ctx.send(
+            await interaction.followup.send(
                 f"{self.config.emotes.fail} `{word}` is already on the whitelist"
             )
             return
@@ -97,51 +115,51 @@ class SlurCommands(nextcord.ext.commands.Cog):
                 word_contains_slur = True
 
         if not word_contains_slur:
-            await ctx.send(
+            await interaction.followup.send(
                 f"{self.config.emotes.fail} `{word}` does not contain any slurs; cannot be added."
             )
             return
 
         for existing_word in get_goodwords():
             if word in existing_word:
-                await ctx.send(
+                await interaction.followup.send(
                     f"{self.config.emotes.fail} `{word}` is substring to existing goodword `{existing_word}`; "
                     "cannot be added."
                 )
                 return
             elif existing_word in word:
-                await ctx.send(
+                await interaction.followup.send(
                     f"{self.config.emotes.fail} existing goodword `{existing_word}` is substring to `{word}`; "
                     "cannot be added."
                 )
                 return
 
-        await ctx.send(f"Goodword to be added: {word}")
+        await interaction.followup.send(f"Goodword to be added: {word}")
 
         with open(self.config.datafiles.goodwordfile, "a") as file:
             file.write(word)
             file.write("\n")
 
-        load_goodwords()  # reloads updated list into memory
+        load_goodwords()
 
         # logging
+
         await self.bot.get_channel(self.config.channels.logging).send(
             embed=SersiEmbed(
                 title="Goodword Added",
-                description="A new goodword has been added to the filter.",
+                description="A new goodword has been added to the whitelist.",
                 fields={
-                    "Added By:": f"{ctx.message.author.mention} ({ctx.message.author.id})",
+                    "Added By:": f"{interaction.user.mention} ({interaction.user.id})",
                     "Goodword Added:": word,
                 },
             )
         )
-        await ctx.send(
+
+        await interaction.followup.send(
             f"{self.config.emotes.success} Goodword added. Detection will start now."
         )
 
-    @nextcord.slash_command(
-        dm_permission=False,
-        guild_ids=[977377117895536640, 856262303795380224],
+    @slur_detection.subcommand(
         description="Remove a slur from the list of slurs.",
     )
     async def remove_slur(
@@ -204,17 +222,34 @@ class SlurCommands(nextcord.ext.commands.Cog):
         await execute(self.bot, self.config, interaction)
 
     @remove_slur.on_autocomplete("slur")
-    async def list_slurs(self, interaction: nextcord.Interaction, input: str):
+    async def get_slurs(self, interaction: nextcord.Interaction, input: str):
         if not is_mod(interaction.user):
             await interaction.response.send_autocomplete([])
 
         slurs = [slur for slur in get_slurs() if slur.lower().startswith(input.lower())]
         await interaction.response.send_autocomplete(slurs)
 
-    @commands.command(aliases=["rmgw", "rmgoodword", "removegw"])
-    async def removegoodword(self, ctx, word):
+    @slur_detection.subcommand(
+        description="Remove a goodword from the whitelist.",
+    )
+    async def remove_goodword(
+        self,
+        interaction: nextcord.Interaction,
+        word: str = nextcord.SlashOption(
+            description="The word to remove from the whitelist.",
+            required=True,
+        ),
+    ):
         """Remove a goodword from the whitelist."""
-        if not await permcheck(ctx, is_mod):
+        if not await permcheck(interaction, is_mod):
+            return
+
+        await interaction.response.defer(ephemeral=False)
+
+        if word not in get_goodwords():
+            await interaction.followup.send(
+                f"{self.config.emotes.fail} {word} is not on the whitelist."
+            )
             return
 
         rm_goodword(word)
@@ -225,23 +260,38 @@ class SlurCommands(nextcord.ext.commands.Cog):
                 title="Goodword Removed",
                 description="A goodword has been removed from the filter.",
                 fields={
-                    "Removed By:": f"{ctx.message.author.mention} ({ctx.message.author.id})",
+                    "Removed By:": f"{interaction.user.mention} ({interaction.user.id})",
                     "Goodword Removed:": word,
                 },
             )
         )
 
-        await ctx.send(
+        await interaction.followup.send(
             f"{self.config.emotes.success} Goodword {word} is no longer in the list"
         )
 
-    @commands.command(aliases=["lssl", "listsl", "lsslurs"])
-    async def listslurs(self, ctx, page=1):
-        """List currently detected slurs.
+    @remove_goodword.on_autocomplete("word")
+    async def get_goodwords(self, interaction: nextcord.Interaction, input: str):
+        if not is_mod(interaction.user):
+            await interaction.response.send_autocomplete([])
 
-        List slurs currently being detected by the bot, 100 slurs listed per page.
-        """
-        if not await permcheck(ctx, is_mod):
+        goodwords = [
+            word for word in get_goodwords() if word.lower().startswith(input.lower())
+        ]
+        await interaction.response.send_autocomplete(goodwords)
+
+    @slur_detection.subcommand(description="List currently detected slurs.")
+    async def list_slurs(
+        self,
+        interaction: nextcord.Interaction,
+        page: int = nextcord.SlashOption(
+            description="The page of slurs to display.",
+            required=False,
+            default=1,
+        ),
+    ):
+        """List currently detected slurs."""
+        if not await permcheck(interaction, is_mod):
             return
 
         embed = SersiEmbed(title="List of currently detected slurs")
@@ -250,19 +300,24 @@ class SlurCommands(nextcord.ext.commands.Cog):
             config=self.config,
             base_embed=embed,
             fetch_function=get_slurs,
-            author=ctx.author,
+            author=interaction.user,
             cols=2,
             init_page=int(page),
         )
-        await view.send_embed(ctx.channel)
+        await view.send_embed(interaction.channel)
 
-    @commands.command(aliases=["lsgw", "lsgoodwords", "listgw"])
-    async def listgoodwords(self, ctx, page=1):
-        """List current goodwords.
-
-        Currently, whitelisted from slur detection, 100 words listed per page.
-        """
-        if not await permcheck(ctx, is_mod):
+    @slur_detection.subcommand(description="List currently whitelisted goodwords.")
+    async def list_goodwords(
+        self,
+        interaction: nextcord.Interaction,
+        page: int = nextcord.SlashOption(
+            description="The page of goodwords to display.",
+            required=False,
+            default=1,
+        ),
+    ):
+        """List currently whitelisted goodwords."""
+        if not await permcheck(interaction, is_mod):
             return
 
         embed = SersiEmbed(
@@ -273,12 +328,12 @@ class SlurCommands(nextcord.ext.commands.Cog):
             config=self.config,
             base_embed=embed,
             fetch_function=get_goodwords,
-            author=ctx.author,
+            author=interaction.user,
             cols=2,
             init_page=int(page),
         )
-        await view.send_embed(ctx.channel)
+        await view.send_embed(interaction.channel)
 
 
-def setup(bot, **kwargs):
+def setup(bot: commands.Bot, **kwargs):
     bot.add_cog(SlurCommands(bot, kwargs["config"]))
