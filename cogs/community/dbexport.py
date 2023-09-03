16b568e62e04f446c7ec1ccfc2c2b81345033243
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2023-09-03 18:15:40 +0200

    [NEW] export database to SQL script

---
+++ cogs/community/dbexport.py
@@ -0,0 +1,38 @@
+import io
+import sqlite3
+
+import nextcord
+from nextcord.ext import commands
+
+from utils.config import Configuration
+from utils.perms import is_sersi_contributor, permcheck
+
+
+class DBExport(commands.Cog):
+    def __init__(self, bot, config: Configuration):
+        self.bot = bot
+        self.config = config
+
+    @nextcord.slash_command(
+        dm_permission=False,
+        guild_ids=[977377117895536640, 856262303795380224],
+        description="Exports the DB into an SQL Script",
+    )
+    async def export(self, interaction: nextcord.Interaction):
+        if not await permcheck(interaction, is_sersi_contributor):
+            return
+        await interaction.response.defer(ephemeral=True)
+
+        buffer = io.StringIO()
+        con = sqlite3.connect(self.config.datafiles.sersi_db)
+
+        for line in con.iterdump():
+            buffer.write("%s\n" % line)
+
+        buffer.seek(0)
+
+        await interaction.followup.send(file=nextcord.File(buffer, "dump.sql"))
+
+
+def setup(bot, **kwargs):
+    bot.add_cog(DBExport(bot, kwargs["config"]))
