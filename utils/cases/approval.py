aca325971099041f2708f79b352ab9fb35fe7fd1
Yoshod <stephenkyle1511@icloud.com>
2023-09-04 11:01:59 +0100

    [UPD] Approval now works with more than warns

--- utils/cases/approval.py
+++ utils/cases/approval.py
@@ -9,9 +9,11 @@ def update_approved(case_id_dirty, config: Configuration):
 
     case_id_clean = case_id_dirty[1:-1]
 
+    case_table = determine_case_table(case_id_clean, config)
+
     cursor.execute(
-        """
-            UPDATE warn_cases
+        f"""
+            UPDATE {case_table}
             SET approved = True
             WHERE id =?""",
         (case_id_clean,),
@@ -27,9 +29,11 @@ def update_objected(case_id_dirty, config: Configuration):
 
     case_id_clean = case_id_dirty[1:-1]
 
+    case_table = determine_case_table(case_id_clean, config)
+
     cursor.execute(
-        """
-            UPDATE warn_cases
+        f"""
+            UPDATE {case_table}
             SET approved = False
             WHERE id =?""",
         (case_id_clean,),
@@ -37,3 +41,31 @@ def update_objected(case_id_dirty, config: Configuration):
 
     conn.commit()
     conn.close()
+
+
+def determine_case_table(case_id, config: Configuration):
+    conn = sqlite3.connect(config.datafiles.sersi_db)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM cases WHERE id=?", (case_id,))
+
+    try:
+        row = cursor.fetchone()
+
+    except TypeError:
+        cursor.close()
+        return None
+
+    case_type: str = row[1]
+
+    match case_type:
+        case "Timeout":
+            return "timeout_cases"
+
+        case "Warn":
+            return "warn_cases"
+
+        case "Kick":
+            return "kick_cases"
+
+        case "Ban":
+            return "ban_cases"
