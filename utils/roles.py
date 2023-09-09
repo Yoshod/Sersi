6e63709cd7822cee51404039b2f8d011862bf7c3
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2023-09-09 18:36:10 +0200

    [NEW] parse_roles fuction instead of add_role/remove_role

--- utils/roles.py
+++ utils/roles.py
@@ -1,73 +1,13 @@
 import nextcord
 
-async def give_role(
-    member: nextcord.Member|int,
-    role: nextcord.Role|int,
-    guild: nextcord.Guild = None,
-    reason: str = None,
-):
-    """
-    Gives a role to a member
 
-    Args:
-        memeber (nextcord.Member|int): Member to give role to
-        role (nextcord.Role|int): Role to give
-        guild (nextcord.Guild, optional): Guild to get member and role from.
-            Defaults to None, required if memeber or role is an int.
-        reason (str, optional): Reason for audit log. Defaults to None.
-        handle_error (callable, optional): Function to handle errors. Defaults to print.
-    """
-
-    if isinstance(member, int) and guild is not None:
-        member = guild.get_member(member)
-    if isinstance(role, int) and guild is not None:
-        role = guild.get_role(role)
-    if member is None or role is None:
-        print("Error: Invalid member or role")
-        return False
-    if role not in member.roles:
-        try:
-            await member.add_roles(role, reason=reason, atomic=True)
-        except nextcord.Forbidden:
-            print(f"Error: Missing permissions to give role {role.name}")
-            return False
-        except nextcord.HTTPException:
-            await print(f"Error: An error occured while giving role {role.name}")
-            return False
-    return True
-
-async def remove_role(
-    member: nextcord.Member|int,
-    role: nextcord.Role|int,
-    guild: nextcord.Guild = None,
-    reason: str = None,
-):
-    """
-    Removes a role from a member
-
-    Args:
-        memeber (nextcord.Member|int): Member to remove role from
-        role (nextcord.Role|int): Role to remove
-        guild (nextcord.Guild, optional): Guild to get member and role from.
-            Defaults to None, required if memeber or role is an int.
-        reason (str, optional): Reason for audit log. Defaults to None.
-        handle_error (callable, optional): Function to handle errors. Defaults to print.
-    """
-
-    if isinstance(member, int) and guild is not None:
-        member = guild.get_member(member)
-    if isinstance(role, int) and guild is not None:
-        role = guild.get_role(role)
-    if member is None or role is None:
-        print("Error: Invalid member or role")
-        return False
-    if role in member.roles:
-        try:
-            await member.remove_roles(role, reason=reason, atomic=True)
-        except nextcord.Forbidden:
-            print(f"Error: Missing permissions to remove role {role.name}")
-            return False
-        except nextcord.HTTPException:
-            print(f"Error: An error occured while removing role {role.name}")
-            return False
-    return True
+def parse_roles(guild: nextcord.Guild, *roles: nextcord.Role|int):
+    """Parses roles for use in role assignment/removal on member."""
+    for role in roles:
+        match (role):
+            case nextcord.Role():
+                yield role
+            case int():
+                role_obj = guild.get_role(role)
+                if role_obj is not None:
+                    yield role_obj
