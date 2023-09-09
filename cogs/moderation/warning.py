131acc39f7650aff9640556b07b83d62dfacbed3
kisachi5 <76996779+kisachi5@users.noreply.github.com>
2023-09-09 12:45:57 +0200

    [NEW] Objection Util

--- cogs/moderation/warning.py
+++ cogs/moderation/warning.py
@@ -1,10 +1,6 @@
-import datetime
-
 import nextcord
 from nextcord.ext import commands
 
-from utils import logs
-from utils.cases.approval import update_approved, update_objected
 from utils.cases.autocomplete import fetch_offences_by_partial_name
 from utils.cases.create import create_warn_case
 from utils.cases.delete import delete_warn
@@ -13,112 +9,16 @@ from utils.cases.fetch import get_case_by_id
 from utils.cases.mend import deactivate_warn
 from utils.cases.misc import offence_validity_check, deletion_validity_check
 from utils.config import Configuration
+from utils.objection import AlertView
 from utils.perms import (
     permcheck,
     is_mod,
     is_dark_mod,
     is_immune,
     target_eligibility,
-    is_compliance,
-    is_senior_mod,
 )
 from utils.review import create_alert
 from utils.sersi_embed import SersiEmbed
-
-
-class ObjectionButton(nextcord.ui.Button):
-    def __init__(self, config: Configuration):
-        super().__init__(label="Object", style=nextcord.ButtonStyle.red)
-        self.config = config
-
-    async def callback(self, interaction: nextcord.Interaction) -> None:
-        new_embed = interaction.message.embeds[0]
-        new_embed.add_field(
-            name="Moderation Action Objected To",
-            value=interaction.user.mention,
-            inline=True,
-        )
-        new_embed.colour = nextcord.Colour.brand_red()
-        await interaction.message.edit(embed=new_embed, view=None)
-
-        # Database
-        update_objected(new_embed.fields[0].value, self.config)
-
-        # Logging
-        await interaction.guild.get_channel(self.config.channels.logging).send(
-            embed=SersiEmbed(
-                title="Moderation Action Objected To",
-                description="A Moderator Action has been objected to by a moderator in response to a report.",
-                fields={
-                    "Report:": interaction.message.jump_url,
-                    "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
-                },
-                footer="Sersi Moderation Peer Review",
-            )
-        )
-
-        await logs.update_response(
-            self.config,
-            interaction.message,
-            datetime.datetime.now(datetime.timezone.utc),
-        )
-
-
-class ApprovalButton(nextcord.ui.Button):
-    def __init__(self, config: Configuration):
-        super().__init__(label="Object", style=nextcord.ButtonStyle.green)
-        self.config = config
-
-    async def callback(self, interaction: nextcord.Interaction) -> None:
-        new_embed: nextcord.Embed = interaction.message.embeds[0]
-        new_embed.add_field(
-            name="Moderation Action Approved",
-            value=interaction.user.mention,
-            inline=True,
-        )
-        new_embed.colour = nextcord.Colour.brand_green()
-        await interaction.message.edit(embed=new_embed, view=None)
-
-        update_approved(new_embed.fields[0].value, self.config)
-
-        # Logging
-        await interaction.guild.get_channel(self.config.channels.logging).send(
-            embed=SersiEmbed(
-                title="Moderation Action Approved",
-                description="A Moderator Action has been approved by a moderator in response to a report.",
-                fields={
-                    "Report:": interaction.message.jump_url,
-                    "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
-                },
-                footer="Sersi Moderation Peer Review",
-            )
-        )
-
-        await logs.update_response(
-            self.config,
-            interaction.message,
-            datetime.datetime.now(datetime.timezone.utc),
-        )
-
-
-class AlertView(nextcord.ui.View):
-    def __init__(self, config: Configuration, reviewer: nextcord.Role):
-        super().__init__(timeout=None)
-        self.config = config
-        self.reviewer = reviewer
-        self.add_item(ApprovalButton(config))
-        self.add_item(ObjectionButton(config))
-
-    async def interaction_check(self, interaction: nextcord.Interaction) -> bool:
-        match self.reviewer:
-            case self.config.permission_roles.compliance:
-                return await permcheck(interaction, is_compliance)
-            case self.config.permission_roles.dark_moderator:
-                return await permcheck(interaction, is_dark_mod)
-            case self.config.permission_roles.senior_moderator:
-                return await permcheck(interaction, is_senior_mod)
-            case self.config.permission_roles.moderator:
-                return await permcheck(interaction, is_mod)
 
 
 class WarningSystem(commands.Cog):
---
+++ utils/objection.py
@@ -0,0 +1,106 @@
+import datetime
+
+import nextcord
+
+from utils import logs
+from utils.cases.approval import update_approved, update_objected
+from utils.config import Configuration
+from utils.perms import permcheck, is_compliance, is_dark_mod, is_senior_mod, is_mod
+from utils.sersi_embed import SersiEmbed
+
+
+class ObjectionButton(nextcord.ui.Button):
+    def __init__(self, config: Configuration):
+        super().__init__(label="Object", style=nextcord.ButtonStyle.red)
+        self.config = config
+
+    async def callback(self, interaction: nextcord.Interaction) -> None:
+        new_embed = interaction.message.embeds[0]
+        new_embed.add_field(
+            name="Moderation Action Objected To",
+            value=interaction.user.mention,
+            inline=True,
+        )
+        new_embed.colour = nextcord.Colour.brand_red()
+        await interaction.message.edit(embed=new_embed, view=None)
+
+        # Database
+        update_objected(new_embed.fields[0].value, self.config)
+
+        # Logging
+        await interaction.guild.get_channel(self.config.channels.logging).send(
+            embed=SersiEmbed(
+                title="Moderation Action Objected To",
+                description="A Moderator Action has been objected to by a moderator in response to a report.",
+                fields={
+                    "Report:": interaction.message.jump_url,
+                    "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
+                },
+                footer="Sersi Moderation Peer Review",
+            )
+        )
+
+        await logs.update_response(
+            self.config,
+            interaction.message,
+            datetime.datetime.now(datetime.timezone.utc),
+        )
+
+
+class ApprovalButton(nextcord.ui.Button):
+    def __init__(self, config: Configuration):
+        super().__init__(label="Approve", style=nextcord.ButtonStyle.green)
+        self.config = config
+
+    async def callback(self, interaction: nextcord.Interaction) -> None:
+        new_embed: nextcord.Embed = interaction.message.embeds[0]
+        new_embed.add_field(
+            name="Moderation Action Approved",
+            value=interaction.user.mention,
+            inline=True,
+        )
+        new_embed.colour = nextcord.Colour.brand_green()
+        await interaction.message.edit(embed=new_embed, view=None)
+
+        update_approved(new_embed.fields[0].value, self.config)
+
+        # Logging
+        await interaction.guild.get_channel(self.config.channels.logging).send(
+            embed=SersiEmbed(
+                title="Moderation Action Approved",
+                description="A Moderator Action has been approved by a moderator in response to a report.",
+                fields={
+                    "Report:": interaction.message.jump_url,
+                    "Moderator:": f"{interaction.user.mention} ({interaction.user.id})",
+                },
+                footer="Sersi Moderation Peer Review",
+            )
+        )
+
+        await logs.update_response(
+            self.config,
+            interaction.message,
+            datetime.datetime.now(datetime.timezone.utc),
+        )
+
+
+class AlertView(nextcord.ui.View):
+    def __init__(self, config: Configuration, reviewer: nextcord.Role):
+        super().__init__(timeout=None)
+        self.config = config
+        self.reviewer = reviewer
+        self.add_item(ApprovalButton(config))
+        self.add_item(ObjectionButton(config))
+
+    async def interaction_check(self, interaction: nextcord.Interaction) -> bool:
+        match self.reviewer.id:
+            case self.config.permission_roles.compliance:
+                return await permcheck(interaction, is_compliance)
+            case self.config.permission_roles.dark_moderator:
+                return await permcheck(interaction, is_dark_mod)
+            case self.config.permission_roles.senior_moderator:
+                return await permcheck(interaction, is_senior_mod)
+            case self.config.permission_roles.moderator:
+                return await permcheck(interaction, is_mod)
+            case _:
+                return False
