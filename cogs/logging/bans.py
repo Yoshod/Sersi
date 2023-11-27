810e011eeedb3a85525943ac0c5f0b4e63f206f1
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2023-11-27 21:02:08 +0100

    [UPD] Ban/Unban logging rework

--- cogs/logging/bans.py
+++ cogs/logging/bans.py
@@ -6,49 +6,53 @@ from utils.config import Configuration
 
 
 class BanUnban(commands.Cog):
-    def __init__(self, bot, config: Configuration):
+    def __init__(self, bot: commands.Bot, config: Configuration):
         self.bot = bot
         self.config = config
-
+    
     @commands.Cog.listener()
-    async def on_member_ban(self, guild: nextcord.Guild, user: nextcord.User):
-        log: nextcord.AuditLogEntry = (
-            await guild.audit_logs(
-                limit=1, action=nextcord.AuditLogAction.ban
-            ).flatten()
-        )[0]
-
-        await guild.get_channel(self.config.channels.ban_unban).send(
-            embed=SersiEmbed(
-                description=f"{user} was banned",
-                fields={
-                    "User Information": f"{user} `{user.id}` {user.mention}",
-                    "Reason": {log.reason},
-                },
-                footer="Sersi Ban/Unban Logging",
-                colour=nextcord.Colour.brand_red(),
-            ).set_author(name=log.user, icon_url=log.user.display_avatar.url)
-        )
-
-    @commands.Cog.listener()
-    async def on_member_unban(self, guild: nextcord.Guild, user: nextcord.User):
-        log: nextcord.AuditLogEntry = (
-            await guild.audit_logs(
-                limit=1, action=nextcord.AuditLogAction.unban
-            ).flatten()
-        )[0]
-
-        await guild.get_channel(self.config.channels.ban_unban).send(
-            embed=SersiEmbed(
-                description=f"{user} was unbanned",
-                fields={
-                    "User Information": f"{user} `{user.id}` {user.mention}",
-                    "Reason": {log.reason},
-                },
-                footer="Sersi Ban/Unban Logging",
-                colour=nextcord.Colour.brand_green(),
-            ).set_author(name=log.user, icon_url=log.user.display_avatar.url)
-        )
+    async def on_guild_audit_log_entry_create(self, entry: nextcord.AuditLogEntry):
+        if entry.action == nextcord.AuditLogAction.ban:
+            target: nextcord.User = await self.bot.fetch_user(entry._target_id)
+            await entry.guild.get_channel(self.config.channels.ban_unban).send(
+                embed=SersiEmbed(
+                    description=f"{target} was banned",
+                    fields={
+                        "User Information": f"{target} `{target.id}` {target.mention}",
+                        "Reason": entry.reason,
+                    },
+                    footer="Sersi Ban/Unban Logging",
+                    thumbnail_url=target.display_avatar.url,
+                    colour=nextcord.Colour.brand_red(),
+                ).set_author(name=entry.user, icon_url=entry.user.display_avatar.url)
+            )
+        elif entry.action == nextcord.AuditLogAction.unban:
+            target: nextcord.User = await self.bot.fetch_user(entry._target_id)
+            await entry.guild.get_channel(self.config.channels.ban_unban).send(
+                embed=SersiEmbed(
+                    description=f"{target} was unbanned",
+                    fields={
+                        "User Information": f"{target} `{target.id}` {target.mention}",
+                        "Reason": entry.reason,
+                    },
+                    footer="Sersi Ban/Unban Logging",
+                    thumbnail_url=target.display_avatar.url,
+                    colour=nextcord.Colour.brand_green(),
+                ).set_author(name=entry.user, icon_url=entry.user.display_avatar.url)
+            )
+        elif entry.action == nextcord.AuditLogAction.kick:
+            target: nextcord.User = await self.bot.fetch_user(entry._target_id)
+            await entry.guild.get_channel(self.config.channels.ban_unban).send(
+                embed=SersiEmbed(
+                    description=f"{target} was kicked",
+                    fields={
+                        "User Information": f"{target} `{target.id}` {target.mention}",
+                        "Reason": entry.reason,
+                    },
+                    thumbnail_url=target.display_avatar.url,
+                    footer="Sersi Ban/Unban Logging",
+                ).set_author(name=entry.user, icon_url=entry.user.display_avatar.url)
+            )
 
 
 def setup(bot: commands.Bot, **kwargs):
