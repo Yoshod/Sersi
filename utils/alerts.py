8863e20b638f83301fac16cfa18928d1af29307e
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2023-12-09 20:35:15 +0100

    [UPD] make alert id from alert message id

--- utils/alerts.py
+++ utils/alerts.py
@@ -1,8 +1,9 @@
 import enum
+from datetime import datetime
 
 import nextcord
 
-from datetime import datetime
+from utils.base import encode_snowflake
 from utils.database import Alert, db_session
 
 
@@ -12,13 +13,11 @@ class AlertType(enum.Enum):
     Toxic = "Toxic Message"
 
 
-async def create_alert_log(
-    message: nextcord.Message,
-    alert_type: AlertType,
-) -> int:
+async def create_alert_log(message: nextcord.Message, alert_type: AlertType):
     """Creates an alert log entry in the database."""
     with db_session() as session:
         alert = Alert(
+            id=encode_snowflake(message.id),
             alert_type=alert_type.value,
             report_url=message.jump_url,
         )
@@ -28,10 +27,15 @@ async def create_alert_log(
         return alert.id
 
 
-async def add_response_time(
-    alert_id: str,
-):
+async def add_response_time(alert: str | int | nextcord.Message):
     """Adds the response time to an alert log entry in the database."""
+    if isinstance(alert, nextcord.Message):
+        alert_id = encode_snowflake(alert.id)
+    elif isinstance(alert, int):
+        alert_id = encode_snowflake(alert)
+    else:
+        alert_id = alert
+
     with db_session() as session:
         alert = session.query(Alert).filter_by(id=alert_id).first()
         alert.response_time = datetime.utcnow()
