e3b3502290015170c45dfb26458951f0c18f73be
Hekkland <stephenkyle1511@icloud.com>
2024-02-06 16:15:49 +0000

    [NEW] #157 Add Staff to DB Function

--- utils/staff.py
+++ utils/staff.py
@@ -1,4 +1,5 @@
 import enum
+from utils.database import db_session, StaffMembers, ActiveStaff, FormerStaff
 
 
 class Branch(enum.Enum):
@@ -19,3 +20,24 @@ class StaffRole(enum.Enum):
     TRIAL_MOD = "Trial Moderator"
     CET_LEAD = "Community Engagement Team Lead"
     CET = "Community Engagement Team Member"
+
+
+def add_staff_to_db(
+    staff_id: int,
+    branch: Branch,
+    role: StaffRole,
+    approver: int,
+):
+    """Adds a staff member to the database."""
+    with db_session() as session:
+        staff = StaffMembers(member=staff_id)
+        session.add(staff)
+
+        active_staff = ActiveStaff(
+            member=staff_id,
+            branch=branch.value,
+            role=role.value,
+            added_by=approver,
+        )
+        session.add(active_staff)
+        session.commit()
