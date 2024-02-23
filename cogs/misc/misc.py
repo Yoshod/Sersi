40efa44a5cb688a95adf23a634d75e9693fe4df2
Hekkland <stephenkyle1511@icloud.com>
2024-02-23 18:44:52 +0000

    [UPD] Refactor miscellaneous commands

--- cogs/misc/misc.py
+++ cogs/misc/misc.py
@@ -11,8 +11,12 @@ class Misc(commands.Cog):
     @nextcord.slash_command(
         dm_permission=False,
         guild_ids=[1166770860787515422, 977377117895536640, 856262303795380224],
-        description="When someone asks an if they can as something",
+        description="Miscellaneous commands",
     )
+    async def misc(self, interaction: nextcord.Interaction):
+        pass
+
+    @misc.subcommand(description="Don't Ask To Ask, Just Ask")
     async def asktoask(self, interaction: nextcord.Interaction):
         await interaction.send(
             embed=SersiEmbed(
