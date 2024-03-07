99c6621d539bf980ff4a597a9d06a27bdf304912
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2024-03-07 23:37:30 +0100

    [NEW] #201 modal dialog system

--- utils/dialog.py
+++ utils/dialog.py
@@ -1,11 +1,13 @@
+from abc import ABC, abstractmethod
 import asyncio
 from asyncio import Future
 from enum import Enum
 from typing import Any, Optional
+from discord import TextInputStyle
 
 import nextcord
 from nextcord import ButtonStyle
-from nextcord.ui import View, Button
+from nextcord.ui import View, Button, Modal, TextInput
 
 from utils.sersi_embed import SersiEmbed
 
@@ -61,7 +63,7 @@ class _DialogView(View):
 
             async def callback(interaction: nextcord.Interaction, *, value=value):
                 self.future.set_result(value)
-                if interaction.message.flags.ephemeral():
+                if interaction.message.flags.ephemeral:
                     await interaction.response.edit_message(view=None)
                 else:
                     await interaction.message.delete()
@@ -92,13 +94,13 @@ async def message_dialog(
     embed: nextcord.Embed = None,
     timeout: int = 180,
     ephemeral: bool = False,
-) -> bool:
+) -> Any:
     """Creates a confirmation dialog for the user to confirm or deny an action."""
-    confirm_future: Future[bool] = asyncio.get_running_loop().create_future()
+    confirm_future: Future[Any] = asyncio.get_running_loop().create_future()
 
     if content is None and embed is None:
         raise ValueError("You must provide either content or an embed.")
-    
+
     view = _DialogView(confirm_future, interaction.user, buttons, timeout)
 
     message = await interaction.send(
@@ -137,7 +139,7 @@ async def confirm(
 
     response = await message_dialog(
         interaction,
-        { true_button.value: True, false_button.value: False },
+        {true_button.value: True, false_button.value: False},
         content=content,
         embed=embed,
         timeout=timeout,
@@ -148,3 +150,88 @@ async def confirm(
         await interaction.send("*Action cancelled!*")
 
     return response
+
+
+class DialogModal(Modal):
+    """
+    Modal class for modal based dialog.
+
+    Attributes:
+    timeout (int, optional): The timeout for the view. Defaults to 180.
+    **kwargs (TextInput): The inputs for the modal, as keyword arguments.
+    """
+
+    def __init__(
+        self,
+        title: str,
+        timeout: int = 300,
+        **kwargs: TextInput,
+    ):
+        super().__init__(title, timeout=timeout)
+
+        for input in kwargs.values():
+            self.add_item(input)
+
+        self.inputs = kwargs
+
+    async def callback(self, interaction: nextcord.Interaction):
+        self.stop()
+
+    async def get_response(self) -> dict[str, str]:
+        """
+        Waits for the user to respond to the modal and returns the inputs as a dictionary.
+
+        Returns:
+        dict[str, str]: The inputs from the user.
+        None: If the user did not respond.
+        """
+        if await self.wait():
+            return None
+        return {name: input.value for name, input in self.inputs.items()}
+
+
+async def modal_dialog(
+    interaction: nextcord.Interaction,
+    title: str,
+    *,
+    timeout: int = 300,
+    **kwargs: TextInput,
+) -> dict[str, str]:
+    """
+    Creates a modal dialog for the user to input data.
+
+    Args:
+    interaction (nextcord.Interaction): The interaction to send the modal to.
+    timeout (int, optional): The timeout for the modal. Defaults to 600.
+    **kwargs (TextInput): The inputs for the modal, as keyword arguments.
+
+    Returns:
+    dict[str, str]: The inputs from the user.
+    None: If the user did not respond.
+    """
+
+    modal = DialogModal(title, timeout=timeout, **kwargs)
+
+    await interaction.response.send_modal(modal)
+
+    return await modal.get_response()
+
+
+class TextField(TextInput):
+    """
+    TextInput class for text field based dialog.
+
+    Attributes:
+    label (str): The label for the text input.
+    placeholder (str, optional): The placeholder for the text input. Defaults to None.
+    required (bool, optional): If ``True``, the user cannot send the form without filling this field. Defaults to True.
+    """
+
+    def __init__(self, label: str, placeholder: str = None, required: bool = True):
+        super().__init__(
+            label,
+            placeholder=placeholder,
+            required=required,
+            max_length=1024,
+            min_length=8 if required else 0,
+        )
