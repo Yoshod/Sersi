9148a9a7416c164c2ecce287bb22f2a669b4adf4
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2025-07-20 19:16:24 +0200

    [WIP] print argumnets for client errors

--- bot.py
+++ bot.py
@@ -10,19 +10,27 @@ from utils.sersi_embed import SersiEmbed
 class SersiBot(Bot):
     def __init__(self):
         super().__init__()
-        self.error_channel = None
+        self.error_channel: nextcord.abc.Messageable = None
 
     async def on_error(self, event_method: str, *args: Any, **kwargs: Any) -> None:
         if not self.error_channel:
             return super().on_error(self, event_method, *args, **kwargs)
+        
+        embed_fields = {
+            "Event": event_method,
+            "Arguments": limit_string(", ".join(args)),
+        }
+
+        for name, arg in kwargs.items():
+            embed_fields[name.upper()] = limit_string(str(arg))
 
         await self.error_channel.send(
             embed=SersiEmbed(
                 title="An Error Has Occurred",
                 description=f"```{limit_string("".join(format_exc()), 4090)}```",
-                # fields=embed_fields,
-                # footer=f"{interaction.guild.name} ({interaction.guild.id})",
-                # footer_icon=interaction.guild.icon.url,
+                fields=embed_fields,
+                footer=f"{self.user.global_name} ({self.user.name})",
+                footer_icon=self.user.avatar.url,
                 colour=nextcord.Color.from_rgb(208, 29, 29),
             )
         )
