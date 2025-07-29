035a26da3ca83538197d1da38024998a7370e2fb
Hekkland <stephenkyle1511@icloud.com>
2025-07-29 17:35:48 +0100

    [UPD] Rework logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,59 +1,68 @@
 import nextcord
 from utils.sersi_embed import SersiEmbed
 from utils.database import guild_db_manager, LoggingChannels
+from utils.language import lang_manager
 
 
-async def create_log(
-    guild: nextcord.Guild,
-    log_category: str,
-    author_id: int | None = None,
-    colour: nextcord.Colour | None = None,
-    description: str | None = None,
-    footer: str | None = None,
-    image_url: str | None = None,
-    thumbnail_url: str | None = None,
-    title: str | None = None,
-    title_url: str | None = None,
-    video_url: str | None = None,
-    fields: dict[str, str] | None = None,
-):
+async def create_log(guild: nextcord.Guild, log_type: str, **kwargs):
     """
-    Create a log message and send it to the appropriate logging channel.
+    Create a log entry in the database for the specified guild and log type.
+
+    :param guild: The guild where the log is being created.
+    :param log_type: The type of log (e.g., 'message_delete', 'user_ban').
+    :param kwargs: Additional keyword arguments to include in the log.
     """
     with guild_db_manager.get_session(guild.id) as session:
-        channel = (
-            session.query(LoggingChannels).filter_by(log_category=log_category).first()
+        logging_channel = (
+            session.query(LoggingChannels).filter_by(log_type=log_type).first()
         )
 
-        if not channel:
+        logging_channel = guild.get_channel(logging_channel.channel_id)
+
+        if not logging_channel:
             return
 
-        channel_id = channel.channel_id
+        match log_type:
+            case "logging_setup_complete":
+                embed = SersiEmbed(
+                    title=lang_manager.get_string(
+                        guild.id, "logging.setup_complete.title"
+                    ),
+                    description=lang_manager.get_string(
+                        guild.id, "logging.setup_complete.description"
+                    ).format(**kwargs),
+                )
 
-        embed = SersiEmbed(
-            title=title,
-            description=description,
-            colour=colour,
-            footer=footer,
-            image=image_url,
-            thumbnail=thumbnail_url,
-            url=title_url,
-            video=video_url,
-            fields=fields,
-        )
+            case "message_delete":
+                embed = SersiEmbed(
+                    description=lang_manager.get_string(
+                        guild.id, "logging.message_delete.description"
+                    ).format(**kwargs),
+                    thumbnail_url=kwargs.get("thumbnail_url", None),
+                    author=kwargs.get("author", None),
+                    footer=lang_manager.get_string(
+                        guild.id, "logging.message_delete.footer"
+                    ),
+                    fields=[
+                        {
+                            lang_manager.get_string(
+                                guild.id,
+                                "logging.message_delete.fields.message_content",
+                            ): kwargs.get("message_content"),
+                            lang_manager.get_string(
+                                guild.id, "logging.message_delete.fields.channel"
+                            ): kwargs.get("channel_id"),
+                            lang_manager.get_string(
+                                guild.id, "logging.message_delete.fields.created"
+                            ): kwargs.get("created_at", None),
+                            lang_manager.get_string(
+                                guild.id, "logging.message_delete.fields.deleted"
+                            ): kwargs.get("deleted_at", None),
+                            lang_manager.get_string(
+                                guild.id, "logging.message_delete.fields.deleted_by"
+                            ): kwargs.get("deleted_by", None),
+                        }
+                    ],
+                )
 
-        if author_id:
-            member = nextcord.utils.get(
-                session.query(nextcord.Member).filter_by(id=author_id).all()
-            )
-            if member:
-                embed.set_author(name=str(member), icon_url=member.avatar.url)
-
-        log_channel = guild.get_channel(channel_id)
-        if log_channel:
-            await log_channel.send(embed=embed)
-        else:
-            print(
-                f"Log channel {channel_id} not found in guild {guild.name} ({guild.id})"
-            )
-            return
+        return await logging_channel.send(embed=embed)
