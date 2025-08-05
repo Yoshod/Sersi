5184947ee7acaebb81efd47ecd06ff20b07dba00
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2025-08-05 21:33:10 +0200

    [OPT] use flat dict for translation

--- utils/language.py
+++ utils/language.py
@@ -1,9 +1,21 @@
 import yaml
 import os
+
 from utils.database import SessionLocal, Language
 
 
+def flatten_dict(dictionary: dict, sep='.', prefix='') -> dict[str, str]:
+    new_dict = dict()
+    for k, v in dictionary.items():
+        if isinstance(v, dict):
+            new_dict.update(flatten_dict(v, sep, k))
+        else:
+            new_dict[prefix + sep + k if prefix else k] = v
+
+
 class LanguageManager:
+    strings: dict[str, dict[str, str]]
+
     def __init__(self, lang_dir="lang"):
         self.strings = {}
         self.lang_dir = lang_dir
@@ -12,15 +24,15 @@ class LanguageManager:
     def load_languages(self):
         """Load all language files from the specified directory."""
         for filename in os.listdir(self.lang_dir):
-            if filename.endswith(".yaml"):
-                lang_code = filename[:-5]
-                with open(
-                    os.path.join(self.lang_dir, filename), "r", encoding="utf-8"
-                ) as file:
-                    self.strings[lang_code] = yaml.safe_load(file)
+            if not filename.endswith(".yaml"):
+                continue
+            lang_code = filename[:-5]
+            with open(
+                os.path.join(self.lang_dir, filename), "r", encoding="utf-8"
+            ) as file:
+                self.strings[lang_code] = flatten_dict(yaml.safe_load(file))
 
-    def get_string(self, guild_id: int, key, **kwargs):
-        keys = key.split(".")
+    def get_string(self, guild_id: int, key: str, **kwargs):
         guild_lang = "en"  # Default to English
 
         with SessionLocal() as session:
@@ -31,22 +43,16 @@ class LanguageManager:
                 guild_lang = result[0]
 
         try:
-            string = self.strings[guild_lang]
-            for k in keys:
-                string = string[k]
-            return string.format(**kwargs)
+            return self.strings[guild_lang][key].format(**kwargs)
         except (KeyError, TypeError):
             # Fallback to English if string not found in the target language
             print(
                 f"Language key '{key}' not found in '{guild_lang}', falling back to English."
             )
             try:
-                string = self.strings["en"]
-                for k in keys:
-                    string = string[k]
-                return string.format(**kwargs)
+                return self.strings["en"][key].format(**kwargs)
             except (KeyError, TypeError):
-                return key  # Return the key itself if not found anywhere
+                return key.upper()  # Return the key itself if not found anywhere
 
 
 lang_manager = LanguageManager()
