c09ff682397026bf8ba1d7a1149888f594f78d34
Gombik <56089830+BrainFuck42@users.noreply.github.com>
2025-08-05 21:52:45 +0200

    [OPT] get_key function

--- utils/language.py
+++ utils/language.py
@@ -4,7 +4,7 @@ import os
 from utils.database import SessionLocal, Language
 
 
-def flatten_dict(dictionary: dict, sep='.', prefix='') -> dict[str, str]:
+def flatten_dict(dictionary: dict, sep=".", prefix="") -> dict[str, str]:
     new_dict = dict()
     for k, v in dictionary.items():
         if isinstance(v, dict):
@@ -32,6 +32,17 @@ class LanguageManager:
             ) as file:
                 self.strings[lang_code] = flatten_dict(yaml.safe_load(file))
 
+    def get_key(self, key: str, lang="en"):
+        try:
+            return self.strings[lang][key]
+        except (KeyError, TypeError):
+            print(f"[ERROR] Language key '{key}' not found in '{lang}.yaml'")
+
+        if lang != "en":
+            return self.get_key(key)
+
+        return key.upper()
+
     def get_string(self, guild_id: int, key: str, **kwargs):
         guild_lang = "en"  # Default to English
 
@@ -42,17 +53,7 @@ class LanguageManager:
             if result:
                 guild_lang = result[0]
 
-        try:
-            return self.strings[guild_lang][key].format(**kwargs)
-        except (KeyError, TypeError):
-            # Fallback to English if string not found in the target language
-            print(
-                f"Language key '{key}' not found in '{guild_lang}', falling back to English."
-            )
-            try:
-                return self.strings["en"][key].format(**kwargs)
-            except (KeyError, TypeError):
-                return key.upper()  # Return the key itself if not found anywhere
+        return self.get_key(key, guild_lang).format(**kwargs)
 
 
 lang_manager = LanguageManager()
